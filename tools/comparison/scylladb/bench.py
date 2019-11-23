5433f6b679bf1854632ae97f81bcefb002677afc
Andreas Meingast <ameingast@gmail.com>
2019-11-23 13:41:21 +0100

    wip: scylla comparison

--- tools/comparison/scylladb/bench.py
+++ tools/comparison/scylladb/bench.py
@@ -1,20 +1,27 @@
 # sequential, non-concurrent inserts with configurable batch size
 
+import timeit
 from cassandra.query import BatchStatement
 from cassandra.cluster import Cluster
 from cassandra import ConsistencyLevel
 
-iterations = 1000
-batch_size = 10
-print("Running", iterations, "iterations at batch size", batch_size)
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect('ks_test')
 stmt = session.prepare('INSERT INTO test (key,value) VALUES (?, ?)')
 batch = BatchStatement(consistency_level=ConsistencyLevel.ANY)
-for k in range(iterations):
-	for i,j in [(str(x), "123456789") for x in range(batch_size)]:
-	    try:
-	      batch.add(stmt,(i,j))
-	    except Exception as e:
-	      print('Cassandra error: {}'.format(e))
-	session.execute(batch)
+
+def run(iterations, batch_size):
+	print("Running", iterations, "iterations at batch size", batch_size)
+	for k in range(iterations):
+		for i,j in [(str(x), "123456789") for x in range(batch_size)]:
+			try:
+			  batch.add(stmt,(i,j))
+			except Exception as e:
+			  print('Cassandra error: {}'.format(e))
+		session.execute(batch)
+		print("Checkpoint", k * batch_size) if k % 10 == 0
+
+duration = timeit.timeit(lambda: run(1000, 10), number=1)
+print(duration, "s", )
+print("Statements", 1000 * 10)
+print("Throughput", 1000 * 10 / duration)
