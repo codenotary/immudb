5b671b3b5db35c368f91246b24aec6ee25d9a5b8
Leonardo Grasso <me@leonardograsso.com>
2019-11-29 11:26:23 +0100

    chore(db): treestore improvements

--- pkg/db/treestore.go
+++ pkg/db/treestore.go
@@ -34,7 +34,8 @@ import (
 	"github.com/dgraph-io/badger/v2"
 )
 
-var tsPrefix = byte(0)
+const tsPrefix = byte(0)
+const bitTreeEntry = byte(255)
 
 func treeKey(layer uint8, index uint64) []byte {
 	k := make([]byte, 1+1+8, 1+1+8)
@@ -135,10 +136,6 @@ func (t *treeStore) makeCaches() {
 	}
 }
 
-func (t *treeStore) replay(upTo uint64) {
-
-}
-
 // Close closes a treeStore. All pending items will be processed and flushed.
 // Calling treeStore.Close() multiple times would still only close the treeStore once.
 func (t *treeStore) Close() {
@@ -167,6 +164,7 @@ func (t *treeStore) WaitUntil(index uint64) {
 }
 
 // NewEntry acquires a lease for a new entry and returns it. The entry must be used with Commit() or Discard().
+// It's thread-safe.
 func (t *treeStore) NewEntry(key []byte, value []byte) *treeStoreEntry {
 	ts := atomic.AddUint64(&t.ts, 1)
 	h := api.Digest(ts-1, key, value)
@@ -177,6 +175,7 @@ func (t *treeStore) NewEntry(key []byte, value []byte) *treeStoreEntry {
 }
 
 // NewBatch is similar to NewEntry but accept a slice of key-value pairs.
+// It's thread-safe.
 func (t *treeStore) NewBatch(kvPairs []KVPair) []*treeStoreEntry {
 	size := uint64(len(kvPairs))
 	batch := make([]*treeStoreEntry, 0, size)
@@ -190,13 +189,13 @@ func (t *treeStore) NewBatch(kvPairs []KVPair) []*treeStoreEntry {
 }
 
 // Commit enqueues the given entry to be included in the tree.
-// Commit will fail if called after Close().
+// It's thread-safe. Commit will fail if called after Close().
 func (t *treeStore) Commit(entry *treeStoreEntry) {
 	t.c <- entry
 }
 
 // Discard enqueues the given entry to be included in the tree as discarded item.
-// Discard will fail if called after Close().
+// It's thread-safe. Discard will fail if called after Close().
 func (t *treeStore) Discard(entry *treeStoreEntry) {
 	h := api.Digest(entry.ts, []byte{}, []byte{})
 	entry.h = &h
@@ -204,14 +203,13 @@ func (t *treeStore) Discard(entry *treeStoreEntry) {
 }
 
 func (t *treeStore) worker() {
-	pqq := make(treeStorePQ, 0, t.cSize)
-	pq := &pqq
+	pq := make(treeStorePQ, 0, t.cSize)
 	for item := range t.c {
-		heap.Push(pq, item)
+		heap.Push(&pq, item)
 
 		t.Lock()
 		for min := pq.Min(); min == t.w+1; min = pq.Min() {
-			tree.AppendHash(t, heap.Pop(pq).(*treeStoreEntry).h)
+			tree.AppendHash(t, heap.Pop(&pq).(*treeStoreEntry).h)
 			if t.w%2 == 0 && (t.w-t.lastFlushed) >= t.cSize/2 {
 				t.flush()
 			}
@@ -227,13 +225,27 @@ func (t *treeStore) worker() {
 	t.quit <- struct{}{}
 }
 
+// flush should be only called when the tree is in a consistent state and _t_ is locked.
+// It always flushes the last portion (ie. items not yet flushed) of buffers in batch,
+// in case of failure previous stored state will be preserved and cache indexes will be not advanced.
 func (t *treeStore) flush() {
 	t.log.Infof("Flushing tree caches at index %d", t.w-1)
+	cancel := false
 	var wb *badger.WriteBatch
 	wb = t.db.NewWriteBatchAt(t.w)
 	defer func() {
+		if cancel {
+			wb.Cancel()
+			return
+		}
 		if err := wb.Flush(); err != nil {
 			t.log.Errorf("Tree flush error: %s", err)
+		} else {
+			// advance cache indexes iff flushing has succeeded
+			for l, c := range t.caches {
+				t.cPos[l] = c.Tail()
+			}
+			t.lastFlushed = t.w
 		}
 	}()
 
@@ -247,15 +259,24 @@ func (t *treeStore) flush() {
 		for i := t.cPos[l]; i < tail; i++ {
 			if h := c.Get(i); h != nil {
 				// fmt.Printf("Storing [l=%d, i=%d]\n", l, i)
-				if err := wb.Set(treeKey(uint8(l), i), h.(*[sha256.Size]byte)[:]); err != nil {
+				entry := badger.Entry{
+					Key:      treeKey(uint8(l), i),
+					Value:    h.(*[sha256.Size]byte)[:],
+					UserMeta: bitTreeEntry,
+				}
+				// it's safe to discard, only non-frozen nodes could be overwritten
+				entry.WithDiscard()
+
+				if err := wb.SetEntry(&entry); err != nil {
+					// fixme(leogr): that should never happen, use panic() ?
 					t.log.Errorf("Cannot flush tree item (l=%d, i=%d): %s", err)
+					t.log.Warningf("Tree flush cancelled")
+					cancel = true
+					return
 				}
 			}
 		}
-		t.cPos[l] = tail
 	}
-
-	t.lastFlushed = t.w
 }
 
 func (t *treeStore) Width() uint64 {
@@ -277,21 +298,19 @@ func (t *treeStore) Get(layer uint8, index uint64) *[sha256.Size]byte {
 	}
 
 	var ret [sha256.Size]byte
-	t.db.View(func(txn *badger.Txn) error {
+	if err := t.db.View(func(txn *badger.Txn) error {
 		// fmt.Printf("CACHE MISS (ts=%d, w=%d, d=%d): [%d,%d]\n", t.ts, t.w, tree.Depth(t), layer, index)
 		item, err := txn.Get(treeKey(layer, index))
 		if err != nil {
-			return nil
+			return err
 		}
-		item.Value(func(val []byte) error {
-			if val != nil {
-				copy(ret[:], val)
-				// fmt.Printf("CACHE FALLBACK (ts=%d, w=%d, d=%d): [%d,%d]\n", t.ts, t.w, tree.Depth(t), layer, index)
-			}
-			return nil
-		})
+		if _, err = item.ValueCopy(ret[:]); err != nil {
+			return err
+		}
 		return nil
-	})
+	}); err != nil {
+		return nil
+	}
 
 	return &ret
 }
