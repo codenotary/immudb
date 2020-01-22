0d6960dfdcf891714f518988a20a9766142a6ecf
Andreas Meingast <ameingast@gmail.com>
2020-01-22 08:17:05 +0100

    feat: client-side performance logs

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -29,6 +29,7 @@ import (
 )
 
 func (c *ImmuClient) Connect() (err error) {
+	start := time.Now()
 	if c.isConnected() {
 		return ErrAlreadyConnected
 	}
@@ -38,10 +39,12 @@ func (c *ImmuClient) Connect() (err error) {
 	if err := c.waitForHealthCheck(); err != nil {
 		return err
 	}
+	c.Logger.Debugf("connected %v in %s", c.Options, time.Since(start))
 	return nil
 }
 
 func (c *ImmuClient) Disconnect() error {
+	start := time.Now()
 	if !c.isConnected() {
 		return ErrNotConnected
 	}
@@ -50,7 +53,7 @@ func (c *ImmuClient) Disconnect() error {
 	}
 	c.serviceClient = nil
 	c.clientConn = nil
-	c.Logger.Debugf("disconnected %v", c.Options)
+	c.Logger.Debugf("disconnected %v in %s", c.Options, time.Since(start))
 	return nil
 }
 
@@ -70,6 +73,7 @@ func (c *ImmuClient) Connected(f func() (interface{}, error)) (interface{}, erro
 }
 
 func (c *ImmuClient) Get(keyReader io.Reader) (*schema.Item, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
@@ -77,10 +81,13 @@ func (c *ImmuClient) Get(keyReader io.Reader) (*schema.Item, error) {
 	if err != nil {
 		return nil, err
 	}
-	return c.serviceClient.Get(context.Background(), &schema.Key{Key: key})
+	result, err := c.serviceClient.Get(context.Background(), &schema.Key{Key: key})
+	c.Logger.Debugf("get finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) GetBatch(keyReaders []io.Reader) (*schema.ItemList, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
@@ -92,10 +99,13 @@ func (c *ImmuClient) GetBatch(keyReaders []io.Reader) (*schema.ItemList, error) 
 		}
 		keys.Keys = append(keys.Keys, &schema.Key{Key: key})
 	}
-	return c.serviceClient.GetBatch(context.Background(), keys)
+	result, err := c.serviceClient.GetBatch(context.Background(), keys)
+	c.Logger.Debugf("get-batch finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) Set(keyReader io.Reader, valueReader io.Reader) (*schema.Index, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
@@ -107,13 +117,16 @@ func (c *ImmuClient) Set(keyReader io.Reader, valueReader io.Reader) (*schema.In
 	if err != nil {
 		return nil, err
 	}
-	return c.serviceClient.Set(context.Background(), &schema.KeyValue{
+	result, err := c.serviceClient.Set(context.Background(), &schema.KeyValue{
 		Key:   key,
 		Value: value,
 	})
+	c.Logger.Debugf("set finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) SetBatch(request *BatchRequest) (*schema.Index, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
@@ -121,40 +134,49 @@ func (c *ImmuClient) SetBatch(request *BatchRequest) (*schema.Index, error) {
 	if err != nil {
 		return nil, err
 	}
-	return c.serviceClient.SetBatch(context.Background(), list)
+	result, err := c.serviceClient.SetBatch(context.Background(), list)
+	c.Logger.Debugf("set-batch finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) Inclusion(index uint64) (*schema.InclusionProof, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
-
-	return c.serviceClient.Inclusion(context.Background(), &schema.Index{
+	result, err := c.serviceClient.Inclusion(context.Background(), &schema.Index{
 		Index: index,
 	})
+	c.Logger.Debugf("inclusion finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) Consistency(index uint64) (*schema.ConsistencyProof, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
-
-	return c.serviceClient.Consistency(context.Background(), &schema.Index{
+	result, err := c.serviceClient.Consistency(context.Background(), &schema.Index{
 		Index: index,
 	})
+	c.Logger.Debugf("consistency finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) ByIndex(index uint64) (*schema.Item, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
-
-	return c.serviceClient.ByIndex(context.Background(), &schema.Index{
+	result, err := c.serviceClient.ByIndex(context.Background(), &schema.Index{
 		Index: index,
 	})
+	c.Logger.Debugf("by-index finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) History(keyReader io.Reader) (*schema.ItemList, error) {
+	start := time.Now()
 	if !c.isConnected() {
 		return nil, ErrNotConnected
 	}
@@ -162,13 +184,15 @@ func (c *ImmuClient) History(keyReader io.Reader) (*schema.ItemList, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	return c.serviceClient.History(context.Background(), &schema.Key{
+	result, err := c.serviceClient.History(context.Background(), &schema.Key{
 		Key: key,
 	})
+	c.Logger.Debugf("history finished in %s", time.Since(start))
+	return result, err
 }
 
 func (c *ImmuClient) HealthCheck() error {
+	start := time.Now()
 	if !c.isConnected() {
 		return ErrNotConnected
 	}
@@ -179,6 +203,7 @@ func (c *ImmuClient) HealthCheck() error {
 	if !response.Status {
 		return ErrHealthCheckFailed
 	}
+	c.Logger.Debugf("health-check finished in %s", time.Since(start))
 	return nil
 }
 
