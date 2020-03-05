b8781186f648c176a5b0ebc1ede8b91642e82386
Michele Meloni <cleaversdev@gmail.com>
2020-03-05 16:03:22 +0100

    feat(pkg/server): add reference and safeReference handlers

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -194,6 +194,24 @@ func (s *ImmuServer) Health(context.Context, *empty.Empty) (*schema.HealthRespon
 	return &schema.HealthResponse{Status: health}, nil
 }
 
+func (s *ImmuServer) Reference(ctx context.Context, refOpts *schema.ReferenceOptions) (index *schema.Index, err error) {
+	index, err = s.Store.Reference(refOpts)
+	if err != nil {
+		return nil, err
+	}
+	s.Logger.Debugf("reference options: %v", refOpts)
+	return index, nil
+}
+
+func (s *ImmuServer) SafeReference(ctx context.Context, safeRefOpts *schema.SafeReferenceOptions) (proof *schema.Proof, err error) {
+	proof, err = s.Store.SafeReference(*safeRefOpts)
+	if err != nil {
+		return nil, err
+	}
+	s.Logger.Debugf("safe reference options: %v", safeRefOpts)
+	return proof, nil
+}
+
 func (s *ImmuServer) installShutdownHandler() {
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
