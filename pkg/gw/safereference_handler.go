862ca415d03e878ed0efb7b4a1ead49ee296618e
Michele Meloni <cleaversdev@gmail.com>
2020-03-05 16:03:53 +0100

    feat(pkg/gw): add reference and safeReference rest endpoint

---
+++ pkg/gw/safereference_handler.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gw
+
+import (
+	"context"
+	"github.com/codenotary/immudb/pkg/api/schema"
+	"github.com/codenotary/immudb/pkg/client"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"net/http"
+	"sync"
+)
+
+type SafeReferenceHandler interface {
+	SafeReference(w http.ResponseWriter, req *http.Request, pathParams map[string]string)
+}
+
+type safeReferenceHandler struct {
+	mux    *runtime.ServeMux
+	client schema.ImmuServiceClient
+	rs     client.RootService
+	sync.RWMutex
+}
+
+func NewSafeReferenceHandler(mux *runtime.ServeMux, client schema.ImmuServiceClient, rs client.RootService) SafeReferenceHandler {
+	return &safeReferenceHandler{
+		mux:    mux,
+		client: client,
+		rs:     rs,
+	}
+}
+
+func (h *safeReferenceHandler) SafeReference(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	ctx, cancel := context.WithCancel(req.Context())
+	defer cancel()
+	inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(h.mux, req)
+
+	rctx, err := runtime.AnnotateContext(ctx, h.mux, req)
+	if err != nil {
+		runtime.HTTPError(ctx, h.mux, outboundMarshaler, w, req, err)
+		return
+	}
+	h.Lock()
+	safeReferenceRequestOverwrite := NewSafeReferenceRequestOverwrite(h.rs)
+	resp, md, err := safeReferenceRequestOverwrite.call(rctx, inboundMarshaler, h.client, req, pathParams)
+
+	ctx = runtime.NewServerMetadataContext(ctx, md)
+	if err != nil {
+		runtime.HTTPError(ctx, h.mux, outboundMarshaler, w, req, err)
+		return
+	}
+	safeReferenceResponseOverwrite := NewSafeReferenceResponseOverwrite(h.rs)
+	if err := safeReferenceResponseOverwrite.call(ctx, h.mux, outboundMarshaler, w, req, resp, h.mux.GetForwardResponseOptions()...); err != nil {
+		runtime.HTTPError(ctx, h.mux, outboundMarshaler, w, req, err)
+	}
+	h.Unlock()
+}
---
+++ pkg/gw/safereference_response_overwrite.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gw
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/codenotary/immudb/pkg/api/schema"
+	"github.com/codenotary/immudb/pkg/client"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+)
+
+type SafeReferenceResponseOverwrite interface {
+	call(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) error
+}
+
+type safeReferenceResponseOverwrite struct {
+	rs client.RootService
+}
+
+func NewSafeReferenceResponseOverwrite(rs client.RootService) SafeReferenceResponseOverwrite {
+	return safeReferenceResponseOverwrite{rs}
+}
+
+func (r safeReferenceResponseOverwrite) call(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, req *http.Request, resp proto.Message, opts ...func(context.Context, http.ResponseWriter, proto.Message) error) error {
+	if p, ok := resp.(*schema.Proof); ok {
+		root, err := r.rs.GetRoot(ctx)
+		if err != nil {
+			return err
+		}
+		w.Header().Set("Content-Type", "application/json")
+		m := make(map[string]bool)
+		// The server-generated leaf SHOULD NOT BE USED for security reasons,
+		// maybe somebody can create a temper leaf.
+		// In this case, we rely on SafeReferenceRequestOverwrite.call that has validated it
+		// already, so p.Leaf and the item's hash are guaranteed to be equal.
+		verified := p.Verify(p.Leaf, *root)
+		m["verified"] = verified
+		newData, _ := json.Marshal(m)
+		if verified {
+			//saving a fresh root
+			tocache := new(schema.Root)
+			tocache.Index = p.Index
+			tocache.Root = p.Root
+			err := r.rs.SetRoot(tocache)
+			if err != nil {
+				return err
+			}
+		}
+		if _, err := w.Write(newData); err != nil {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
