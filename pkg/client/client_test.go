e6443a1334f1d1dd900c2c262833a6dea993892b
Valentin Padurean <912518+padurean@users.noreply.github.com>
2020-04-02 21:37:30 +0300

    test(pkg/client): improve automated testing for immu client

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -54,22 +54,7 @@ func bufDialer(ctx context.Context, address string) (net.Conn, error) {
 	return lis.Dial()
 }
 
-func TestImmuClient(t *testing.T) {
-	ctx := context.Background()
-	key := []byte("key1")
-	value := []byte("value1")
-	r, err := client.Connected(ctx, func() (interface{}, error) {
-		_, err2 := client.SafeSet(ctx, key, value)
-		require.NoError(t, err2)
-		return client.SafeGet(ctx, key)
-	})
-	vi := r.(*VerifiedItem)
-	require.NoError(t, err)
-	require.NotNil(t, vi)
-	require.Equal(t, key, vi.Key)
-	require.Equal(t, value, vi.Value)
-	require.True(t, vi.Verified)
-
+func cleanup() {
 	// delete client file and server folder
 	if err := os.Remove(".root"); err != nil {
 		log.Println(err)
@@ -77,4 +62,78 @@ func TestImmuClient(t *testing.T) {
 	if err := os.RemoveAll("immudb"); err != nil {
 		log.Println(err)
 	}
+}
+
+func testSafeSetAndSafeGet(ctx context.Context, t *testing.T, key []byte, value []byte) {
+	r, err := client.Connected(ctx, func() (interface{}, error) {
+		_, err2 := client.SafeSet(ctx, key, value)
+		require.NoError(t, err2)
+		return client.SafeGet(ctx, key)
+	})
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	vi := r.(*VerifiedItem)
+	require.Equal(t, key, vi.Key)
+	require.Equal(t, value, vi.Value)
+	require.True(t, vi.Verified)
+}
+
+func testSafeReference(ctx context.Context, t *testing.T, referenceKey []byte, key []byte, value []byte) {
+	r, err := client.Connected(ctx, func() (interface{}, error) {
+		_, err2 := client.SafeReference(ctx, referenceKey, key)
+		require.NoError(t, err2)
+		return client.SafeGet(ctx, referenceKey)
+	})
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	vi := r.(*VerifiedItem)
+	require.Equal(t, key, vi.Key)
+	require.Equal(t, value, vi.Value)
+	require.True(t, vi.Verified)
+}
+
+func TestImmuClient(t *testing.T) {
+	defer cleanup()
+
+	ctx := context.Background()
+
+	key1 := []byte("key1")
+	key2 := []byte("key2")
+	value1 := []byte("value1")
+	value2 := []byte("value2")
+	testSafeSetAndSafeGet(ctx, t, key1, value1)
+	testSafeSetAndSafeGet(ctx, t, key2, value2)
+
+	refKey1 := []byte("refKey1")
+	refKey2 := []byte("refKey2")
+	testSafeReference(ctx, t, refKey1, key1, value1)
+	testSafeReference(ctx, t, refKey2, key2, value2)
+
+	set := []byte("set1")
+	score1, score2, score3 := 1.0, 2.0, 3.0
+	key3 := []byte("key3")
+	value3 := []byte("value3")
+	r3, err := client.Connected(ctx, func() (interface{}, error) {
+		_, err2 := client.SafeSet(ctx, key3, value3)
+		require.NoError(t, err2)
+
+		_, err2 = client.SafeZAdd(ctx, set, score1, key1)
+		require.NoError(t, err2)
+		_, err2 = client.SafeZAdd(ctx, set, score2, key2)
+		require.NoError(t, err2)
+		_, err2 = client.SafeZAdd(ctx, set, score3, key3)
+		require.NoError(t, err2)
+
+		return client.ZScan(ctx, set)
+	})
+	require.NoError(t, err)
+	require.NotNil(t, r3)
+	itemList := r3.(*schema.ItemList)
+	require.Len(t, itemList.Items, 3)
+	require.Equal(t, key1, itemList.Items[0].Key)
+	require.Equal(t, value1, itemList.Items[0].Value)
+	require.Equal(t, key2, itemList.Items[1].Key)
+	require.Equal(t, value2, itemList.Items[1].Value)
+	require.Equal(t, key3, itemList.Items[2].Key)
+	require.Equal(t, value3, itemList.Items[2].Value)
 }
