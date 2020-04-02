ac24eaa248808181bdc7049e359d2b635fdaf952
Michele Meloni <cleaversdev@gmail.com>
2020-04-02 11:31:24 +0200

    chore(cmd/immugw): Use default options values

--- cmd/immugw/immugw.go
+++ cmd/immugw/immugw.go
@@ -15,16 +15,69 @@ func main() {
 		Short: "Immu gateway",
 		Long:  `Immu gateway is an intelligent proxy for immudb. It expose all gRPC methods with a rest interface and wrap SAFESET and SAFEGET endpoints with a verification service.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return serve(cmd, args)
+			port, err := cmd.Flags().GetInt("port")
+			if err != nil {
+				return err
+			}
+			address, err := cmd.Flags().GetString("address")
+			if err != nil {
+				return err
+			}
+			immudport, err := cmd.Flags().GetInt("immudport")
+			if err != nil {
+				return err
+			}
+			immudAddress, err := cmd.Flags().GetString("immudaddress")
+			if err != nil {
+				return err
+			}
+			mtls, err := cmd.Flags().GetBool("mtls")
+			if err != nil {
+				return err
+			}
+			servername, err := cmd.Flags().GetString("servername")
+			if err != nil {
+				return err
+			}
+			certificate, err := cmd.Flags().GetString("certificate")
+			if err != nil {
+				return err
+			}
+			pkey, err := cmd.Flags().GetString("pkey")
+			if err != nil {
+				return err
+			}
+			client_cas, err := cmd.Flags().GetString("clientcas")
+			if err != nil {
+				return err
+			}
+			options := gw.DefaultOptions().
+				WithPort(port).
+				WithAddress(address).
+				WithImmudAddress(immudAddress).
+				WithImmudPort(immudport).
+				WithMTLs(mtls)
+			if mtls {
+				// todo https://golang.org/src/crypto/x509/root_linux.go
+				options.MTLsOptions = client.DefaultMTLsOptions().
+					WithServername(servername).
+					WithCertificate(certificate).
+					WithPkey(pkey).
+					WithClientCAs(client_cas)
+			}
+			immuGwServer := gw.
+				DefaultServer().
+				WithOptions(options)
+			return immuGwServer.Start()
 		},
 	}
 
-	immugwCmd.Flags().IntP("port", "p", 8081, "immugw port number")
-	immugwCmd.Flags().StringP("address", "s", "127.0.0.1", "immugw host address")
-	immugwCmd.Flags().IntP("immudport", "j", 8080, "immudb port number")
-	immugwCmd.Flags().StringP("immudaddress", "y", "127.0.0.1", "immudb host address")
+	immugwCmd.Flags().IntP("port", "p", gw.DefaultOptions().Port, "immugw port number")
+	immugwCmd.Flags().StringP("address", "s", gw.DefaultOptions().Address, "immugw host address")
+	immugwCmd.Flags().IntP("immudport", "j", gw.DefaultOptions().ImmudPort, "immudb port number")
+	immugwCmd.Flags().StringP("immudaddress", "y", gw.DefaultOptions().ImmudAddress, "immudb host address")
 
-	immugwCmd.Flags().BoolP("mtls", "m", false, "enable mutual tls")
+	immugwCmd.Flags().BoolP("mtls", "m", gw.DefaultOptions().MTLs, "enable mutual tls")
 	immugwCmd.Flags().String("servername", client.DefaultMTLsOptions().Servername, "used to verify the hostname on the returned certificates")
 	immugwCmd.Flags().String("certificate", client.DefaultMTLsOptions().Certificate, "server certificate file path")
 	immugwCmd.Flags().String("pkey", client.DefaultMTLsOptions().Pkey, "server private key path")
@@ -34,61 +87,4 @@ func main() {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func serve(cmd *cobra.Command, args []string) error {
-	port, err := cmd.Flags().GetInt("port")
-	if err != nil {
-		return err
-	}
-	address, err := cmd.Flags().GetString("address")
-	if err != nil {
-		return err
-	}
-	immudport, err := cmd.Flags().GetInt("immudport")
-	if err != nil {
-		return err
-	}
-	immudAddress, err := cmd.Flags().GetString("immudaddress")
-	if err != nil {
-		return err
-	}
-	mtls, err := cmd.Flags().GetBool("mtls")
-	if err != nil {
-		return err
-	}
-	servername, err := cmd.Flags().GetString("servername")
-	if err != nil {
-		return err
-	}
-	certificate, err := cmd.Flags().GetString("certificate")
-	if err != nil {
-		return err
-	}
-	pkey, err := cmd.Flags().GetString("pkey")
-	if err != nil {
-		return err
-	}
-	client_cas, err := cmd.Flags().GetString("clientcas")
-	if err != nil {
-		return err
-	}
-	options := gw.DefaultOptions().
-		WithPort(port).
-		WithAddress(address).
-		WithImmudAddress(immudAddress).
-		WithImmudPort(immudport).
-		WithMTLs(mtls)
-	if mtls {
-		// todo https://golang.org/src/crypto/x509/root_linux.go
-		options.MTLsOptions = client.DefaultMTLsOptions().
-			WithServername(servername).
-			WithCertificate(certificate).
-			WithPkey(pkey).
-			WithClientCAs(client_cas)
-	}
-	immuGwServer := gw.
-		DefaultServer().
-		WithOptions(options)
-	return immuGwServer.Start()
 }
