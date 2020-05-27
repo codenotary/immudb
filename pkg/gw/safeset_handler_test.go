543e09041e22a911632eb4a6a59ab4458b16aaec
gjergji <gjergjiramku@gmail.com>
2020-05-27 14:20:20 +0200

    fix: safeset_handler test

--- pkg/gw/safeset_handler_test.go
+++ pkg/gw/safeset_handler_test.go
@@ -27,15 +27,15 @@ func generateRandomTCPPort() int {
 func TestSafeset(t *testing.T) {
 	key := base64.StdEncoding.EncodeToString([]byte("Pablo"))
 	value := base64.StdEncoding.EncodeToString([]byte("Picasso"))
-
-	op := immudb.DefaultOptions().WithPort(generateRandomTCPPort()).WithDir("db_" + strconv.FormatInt(int64(generateRandomTCPPort()), 10))
+	tcpPort := generateRandomTCPPort()
+	op := immudb.DefaultOptions().WithPort(tcpPort).WithDir("db_" + strconv.FormatInt(int64(tcpPort), 10))
 	s := immudb.DefaultServer().WithOptions(op)
 	go s.Start()
 	time.Sleep(2 * time.Second)
 	defer s.Stop()
 	defer os.RemoveAll(op.Dir)
 
-	ic, err := immuclient.NewImmuClient(immuclient.DefaultOptions())
+	ic, err := immuclient.NewImmuClient(immuclient.DefaultOptions().WithPort(tcpPort))
 	if err != nil {
 		t.Errorf("unable to instantiate client: %s", err)
 		return
