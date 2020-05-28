0a55097c69cf84da0fc7971c504cc2622a9556d8
Michele Meloni <cleaversdev@gmail.com>
2020-05-28 16:22:22 +0200

    chore(cmd/helper): add path os wildcard resolver

---
+++ cmd/helper/config_pathmanager_windows.go
@@ -0,0 +1,54 @@
+// +build windows
+
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"golang.org/x/sys/windows"
+	"strings"
+)
+
+var (
+	cfg_programdata  = windows.FOLDERID_ProgramData
+	cfg_programfiles = windows.FOLDERID_ProgramFiles
+)
+
+// ResolvePath
+func ResolvePath(path string, quote bool) (finalPath string, err error) {
+	var toReplace string
+	var folderId *windows.KNOWNFOLDERID
+	var token string
+	if strings.Contains(path, "%programdata%") {
+		toReplace = "%programdata%"
+		folderId = windows.FOLDERID_ProgramData
+	}
+	if strings.Contains(path, "%programfile%") {
+		toReplace = "%programfile%"
+		folderId = windows.FOLDERID_ProgramFiles
+	}
+	if toReplace != "" {
+		if token, err = windows.KnownFolderPath(folderId, windows.KF_FLAG_DEFAULT); err != nil {
+			return "", err
+		}
+		if quote {
+			token = strings.Replace(token, "\\", "\\\\", -1)
+		}
+		path = strings.Replace(path, toReplace, token, -1)
+	}
+	return path, nil
+}
