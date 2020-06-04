57a7b7e329d8d9a3906b263f95044804262bb3dc
Michele Meloni <cleaversdev@gmail.com>
2020-06-05 00:32:09 +0200

    feat(cmd/helper): add functionalities to print colored output

---
+++ cmd/helper/color.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package helper
+
+import (
+	"fmt"
+	"runtime"
+)
+
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Purple = "\033[35m"
+var Cyan = "\033[36m"
+var White = "\033[37m"
+
+func init() {
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Red = ""
+		Green = ""
+		Yellow = ""
+		Blue = ""
+		Purple = ""
+		Cyan = ""
+		White = ""
+	}
+}
+
+func PrintfColor(color string, format string, args ...interface{}) {
+	fmt.Printf(color+format+Reset, args...)
+}
