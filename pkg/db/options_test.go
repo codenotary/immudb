80b70906f9111da703c7e5befb3c650eab298154
gjergji <gjergjiramku@gmail.com>
2020-06-04 18:20:39 +0200

    feat: db object + tests

---
+++ pkg/db/options_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestDefaultOptions(t *testing.T) {
+	op := DefaultOption()
+	if !strings.Contains(op.DbName, "db_") {
+		t.Errorf("default db dir non as per convention")
+	}
+	if op.GetDbDir() != "immudb" {
+		t.Errorf("default db name non as per convention")
+	}
+	if op.GetSysDbDir() != "immudbsys" {
+		t.Errorf("default sysdb name non as per convention")
+	}
+}
+
+func TestSetOptions(t *testing.T) {
+	DbName := "Charles_Aznavour"
+	op := DefaultOption().WithDbName(DbName)
+	if op.DbName != DbName {
+		t.Errorf("db name not set correctly , expected %s got %s", DbName, op.DbName)
+	}
+}
