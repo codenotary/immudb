edc7780a3dc40e2dcd52cd9e3d02c63475152a58
Valentin Padurean <912518+padurean@users.noreply.github.com>
2020-07-08 14:27:55 +0300

    test: add in memory cache for client roots

---
+++ pkg/client/cache/inmemory_cache_test.go
@@ -0,0 +1,40 @@
+package cache
+
+import (
+	"testing"
+
+	"github.com/codenotary/immudb/pkg/api/schema"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInMemoryCache(t *testing.T) {
+	imc := NewInMemoryCache()
+	require.IsType(t, &inMemoryCache{}, imc)
+
+	err := imc.Set(&schema.Root{Index: 11, Root: []byte{11}}, "server1", "db11")
+	require.NoError(t, err)
+	err = imc.Set(&schema.Root{Index: 12, Root: []byte{12}}, "server1", "db12")
+	require.NoError(t, err)
+	err = imc.Set(&schema.Root{Index: 21, Root: []byte{21}}, "server2", "db21")
+	require.NoError(t, err)
+
+	root, err := imc.Get("server1", "db11")
+	require.NoError(t, err)
+	require.Equal(t, uint64(11), root.Index)
+	require.Equal(t, []byte{11}, root.Root)
+
+	root, err = imc.Get("server1", "db12")
+	require.NoError(t, err)
+	require.Equal(t, uint64(12), root.Index)
+	require.Equal(t, []byte{12}, root.Root)
+
+	root, err = imc.Get("server2", "db21")
+	require.NoError(t, err)
+	require.Equal(t, uint64(21), root.Index)
+	require.Equal(t, []byte{21}, root.Root)
+
+	_, err = imc.Get("unknownServer", "db11")
+	require.Error(t, err)
+	_, err = imc.Get("server1", "unknownDb")
+	require.Error(t, err)
+}
