03719803fb8c5f871c642950df35ccfa11212c65
Michele Meloni <cleaversdev@gmail.com>
2020-07-17 10:13:53 +0200

    chore(cmd/immuclient/command): remove useless comment
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- cmd/immuclient/command/login_test.go
+++ cmd/immuclient/command/login_test.go
@@ -37,7 +37,6 @@ func TestLogin(t *testing.T) {
 		Pass: []string{"immudb"},
 	}, &test.HomedirServiceMock{})
 	ic.Connect(bs.Dialer)
-	//ic.Login("immudb")
 
 	cmdl := commandline{
 		immucl: ic.Imc,
