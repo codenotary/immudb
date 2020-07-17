a9ab8eb15b3c3b241b5e42268fe817548ba9ad4b
Michele Meloni <cleaversdev@gmail.com>
2020-07-17 15:42:06 +0200

    refactor: refactor detach() method in a process launcher service
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

---
+++ cmd/helper/detached_test.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+type execsmock struct{}
+
+func (e execsmock) Command(name string, arg ...string) *exec.Cmd {
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	return cmd
+}
+
+func TestDetached(t *testing.T) {
+	pl := plauncher{execsmock{}}
+	err := pl.Detached()
+	assert.Nil(t, err)
+}
+
+func TestNewPlauncher(t *testing.T) {
+	pl := NewPlauncher()
+	assert.IsType(t, &plauncher{}, pl)
+}
+
+func TestExecs_Command(t *testing.T) {
+	ex := execs{}
+	c := ex.Command("tr", "a-z", "A-Z")
+	assert.IsType(t, &exec.Cmd{}, c)
+}
