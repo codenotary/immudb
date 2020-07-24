80fde8d4958ddc7fbf1276cc04faec97e4286dd1
Michele Meloni <cleaversdev@gmail.com>
2020-07-24 18:04:39 +0200

    test(pkg/server): increase corruption checker code coverage
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- pkg/server/corruption_checker_test.go
+++ pkg/server/corruption_checker_test.go
@@ -18,6 +18,12 @@ package server
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
+	"github.com/dgraph-io/badger/v2"
+	"log"
+	"math"
+	"os"
 	"strconv"
 	"testing"
 	"time"
@@ -39,7 +45,7 @@ func TestEmptyDBCorruptionChecker(t *testing.T) {
 	cco.frequencySleepTime = 1 * time.Millisecond
 	cco.singleiteration = true
 
-	cc := NewCorruptionChecker(cco, dbList, &mockLogger{})
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
 
 	err = cc.Start(context.TODO())
 
@@ -67,7 +73,7 @@ func TestCorruptionChecker(t *testing.T) {
 	cco.frequencySleepTime = 1 * time.Millisecond
 	cco.singleiteration = true
 
-	cc := NewCorruptionChecker(cco, dbList, &mockLogger{})
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
 
 	err = cc.Start(context.TODO())
 
@@ -77,12 +83,298 @@ func TestCorruptionChecker(t *testing.T) {
 	}
 	assert.Nil(t, err)
 }
+
+func TestCorruptionCheckerOnTamperInsertionOrderIndexDb(t *testing.T) {
+	var err error
+	defer os.RemoveAll("test")
+	dbList := NewDatabaseList()
+	options := DefaultOption().WithDbName("test").WithDbRootPath("test")
+	db, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	k := []byte(strconv.FormatUint(1, 10))
+	v := []byte(strconv.FormatUint(2, 10))
+	kv := &schema.KeyValue{
+		Key:   k,
+		Value: v,
+	}
+	if _, err = db.Set(kv); err != nil {
+		log.Fatal(err)
+	}
+	db.Store.Close()
+	// Tampering
+	opts := badger.DefaultOptions("test/test").WithLogger(nil)
+	dbb, err := badger.OpenManaged(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	txn := dbb.NewTransactionAt(math.MaxUint64, true)
+	defer txn.Discard()
+	item, err := txn.Get(k)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ts := item.Version()
+	v1 := []byte(strconv.FormatUint(3, 10))
+	if err := txn.Set(k, v1); err != nil {
+		log.Fatal(err)
+	}
+	if err := txn.CommitAt(ts, nil); err != nil {
+		log.Fatal(err)
+	}
+	dbb.Close()
+	// End Tampering
+	db1, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	dbList.Append(db1)
+
+	time.Sleep(500 * time.Millisecond)
+	cco := CCOptions{}
+	cco.iterationSleepTime = 1 * time.Millisecond
+	cco.frequencySleepTime = 1 * time.Millisecond
+	cco.singleiteration = true
+
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
+
+	err = cc.Start(context.TODO())
+
+	for i := 0; i < dbList.Length(); i++ {
+		val := dbList.GetByIndex(int64(i))
+		val.Store.Close()
+	}
+	assert.Error(t, err)
+}
+
+func TestCorruptionCheckerOnTamperDbInconsistentState(t *testing.T) {
+	var err error
+	defer os.RemoveAll("test")
+	dbList := NewDatabaseList()
+	options := DefaultOption().WithDbName("test").WithDbRootPath("test")
+	db, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	k := []byte(strconv.FormatUint(1, 10))
+	v := []byte(strconv.FormatUint(2, 10))
+	kv := &schema.KeyValue{
+		Key:   k,
+		Value: v,
+	}
+	if _, err = db.Set(kv); err != nil {
+		log.Fatal(err)
+	}
+	db.Store.Close()
+	// Tampering
+	opts := badger.DefaultOptions("test/test").WithLogger(nil)
+	dbb, err := badger.OpenManaged(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	txn := dbb.NewTransactionAt(math.MaxUint64, true)
+	defer txn.Discard()
+
+	item, err := txn.Get(treeKey(0, 0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ts := item.Version()
+	v1 := []byte(strconv.FormatUint(3, 10))
+	if err := txn.Set(item.Key(), v1); err != nil {
+		log.Fatal(err)
+	}
+	if err := txn.CommitAt(ts, nil); err != nil {
+		log.Fatal(err)
+	}
+	dbb.Close()
+	// End Tampering
+	db1, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	dbList.Append(db1)
+
+	time.Sleep(500 * time.Millisecond)
+	cco := CCOptions{}
+	cco.iterationSleepTime = 1 * time.Millisecond
+	cco.frequencySleepTime = 1 * time.Millisecond
+	cco.singleiteration = true
+
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
+
+	err = cc.Start(context.TODO())
+
+	for i := 0; i < dbList.Length(); i++ {
+		val := dbList.GetByIndex(int64(i))
+		val.Store.Close()
+	}
+	assert.Error(t, err)
+}
+
+func TestCorruptionCheckerOnTamperDb(t *testing.T) {
+	var err error
+	defer os.RemoveAll("test")
+	dbList := NewDatabaseList()
+	options := DefaultOption().WithDbName("test").WithDbRootPath("test")
+	db, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	if err != nil {
+		log.Fatal(err)
+	}
+	k := []byte(strconv.FormatUint(1, 10))
+	v := []byte(strconv.FormatUint(2, 10))
+	kv := &schema.KeyValue{
+		Key:   k,
+		Value: v,
+	}
+	if _, err = db.Set(kv); err != nil {
+		log.Fatal(err)
+	}
+	k1 := []byte(strconv.FormatUint(3, 10))
+	v1 := []byte(strconv.FormatUint(4, 10))
+	kv1 := &schema.KeyValue{
+		Key:   k1,
+		Value: v1,
+	}
+	if _, err = db.Set(kv1); err != nil {
+		log.Fatal(err)
+	}
+	db.Store.Close()
+	// Tampering
+	opts := badger.DefaultOptions("test/test").WithLogger(nil)
+	dbb, err := badger.OpenManaged(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	txn := dbb.NewTransactionAt(math.MaxUint64, true)
+	defer txn.Discard()
+
+	item, err := txn.Get(treeKey(0, 0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ts := item.Version()
+	v1 = []byte(`QWERTYUIOPASDFGHJKLZXCBVBN123456fake root`)
+	if err := txn.Set(item.Key(), v1); err != nil {
+		log.Fatal(err)
+	}
+	if err := txn.CommitAt(ts, nil); err != nil {
+		log.Fatal(err)
+	}
+	dbb.Close()
+	// End Tampering
+	db1, err := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	dbList.Append(db1)
+
+	time.Sleep(500 * time.Millisecond)
+	cco := CCOptions{}
+	cco.iterationSleepTime = 1 * time.Millisecond
+	cco.frequencySleepTime = 1 * time.Millisecond
+	cco.singleiteration = true
+
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGeneratorMock{})
+
+	err = cc.Start(context.TODO())
+
+	for i := 0; i < dbList.Length(); i++ {
+		val := dbList.GetByIndex(int64(i))
+		val.Store.Close()
+	}
+	assert.False(t, cc.GetStatus())
+}
+
+type randomGeneratorMock struct{}
+
+func (rg randomGeneratorMock) getList(start, end uint64) []uint64 {
+	ids := make([]uint64, 1)
+	ids[0] = 1
+	return ids
+}
+
+func TestCorruptionChecker_Stop(t *testing.T) {
+	defer os.RemoveAll("test")
+	dbList := NewDatabaseList()
+	options := DefaultOption().WithDbName("test").WithDbRootPath("test")
+	// End Tampering
+	db1, _ := NewDb(options, logger.NewSimpleLogger("immudb ", os.Stderr))
+	dbList.Append(db1)
+
+	time.Sleep(500 * time.Millisecond)
+	cco := CCOptions{}
+	cco.iterationSleepTime = 1 * time.Millisecond
+	cco.frequencySleepTime = 1 * time.Millisecond
+	cco.singleiteration = true
+
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
+
+	cc.Start(context.TODO())
+
+	for i := 0; i < dbList.Length(); i++ {
+		val := dbList.GetByIndex(int64(i))
+		val.Store.Close()
+	}
+	cc.Stop()
+	assert.True(t, cc.GetStatus())
+}
+
+func TestCorruptionChecker_ExitImmediatly(t *testing.T) {
+	var err error
+	dbList := NewDatabaseList()
+	db, _ := makeDb()
+	kv := &schema.KeyValue{
+		Key:   []byte(strconv.FormatUint(1, 10)),
+		Value: []byte(strconv.FormatUint(2, 10)),
+	}
+	db.Set(kv)
+	dbList.Append(db)
+
+	time.Sleep(500 * time.Millisecond)
+	cco := CCOptions{}
+	cco.iterationSleepTime = 1 * time.Millisecond
+	cco.frequencySleepTime = 1 * time.Millisecond
+	cco.singleiteration = true
+
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
+	cc.SetExit(true)
+	err = cc.Start(context.TODO())
+
+	for i := 0; i < dbList.Length(); i++ {
+		val := dbList.GetByIndex(int64(i))
+		val.Store.Close()
+	}
+	assert.Nil(t, err)
+}
+
+func TestCorruptionChecker_GetExit(t *testing.T) {
+	dbList := NewDatabaseList()
+	cco := CCOptions{}
+	cc := NewCorruptionChecker(cco, dbList, &mockLogger{}, randomGenerator{})
+	exit := cc.GetExit()
+	assert.False(t, exit)
+
+}
+
+func treeKey(layer uint8, index uint64) []byte {
+	k := make([]byte, 1+1+8)
+	k[0] = 0
+	k[1] = layer
+	binary.BigEndian.PutUint64(k[2:], index)
+	return k
+}
+
 func TestInt63(t *testing.T) {
 	rand := newCryptoRandSource()
 	n := rand.Int63()
 	if n == 0 {
 		t.Fatal("cryptorand source faild")
 	}
+}
+
+func makeDB(dir string) *badger.DB {
+	opts := badger.DefaultOptions(dir).
+		WithLogger(nil)
+
+	db, err := badger.OpenManaged(opts)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	return db
 }
 
 type mockLogger struct{}
@@ -96,3 +388,8 @@ func (l *mockLogger) Infof(f string, v ...interface{}) {}
 func (l *mockLogger) Debugf(f string, v ...interface{}) {}
 
 func (l *mockLogger) CloneWithLevel(level logger.LogLevel) logger.Logger { return l }
+
+func TestCryptoRandSource_Seed(t *testing.T) {
+	cs := newCryptoRandSource()
+	cs.Seed(678)
+}
