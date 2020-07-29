14021a216b917e3d0857a0eb0d8b2dd9a0ca3334
Valentin Padurean <912518+padurean@users.noreply.github.com>
2020-07-29 20:09:52 +0300

    test(cmd/immuclient): increase coverage for immuclient interractive references, scanners, set and misc commands
    Signed-off-by: Valentin Padurean <912518+padurean@users.noreply.github.com>

---
+++ cmd/immuclient/immuc/misc_errors_test.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package immuc
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/codenotary/immudb/pkg/api/schema"
+	"github.com/codenotary/immudb/pkg/client/clienttest"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestMiscErrors(t *testing.T) {
+	immuClientMock := &clienttest.ImmuClientMock{}
+	ic := &immuc{ImmuClient: immuClientMock}
+
+	// History errors
+	args := []string{"key1"}
+	immuClientMock.HistoryF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, status.New(codes.Internal, "history RPC error").Err()
+	}
+	resp, err := ic.History(args)
+	require.NoError(t, err)
+	require.Equal(t, " history RPC error", resp)
+
+	errHistory := errors.New("history error")
+	immuClientMock.HistoryF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, errHistory
+	}
+	_, err = ic.History(args)
+	require.Equal(t, errHistory, err)
+
+	// HealthCheck errors
+	immuClientMock.HealthCheckF = func(context.Context) error {
+		return status.New(codes.Internal, "health check RPC error").Err()
+	}
+	resp, err = ic.HealthCheck(nil)
+	require.NoError(t, err)
+	require.Equal(t, " health check RPC error", resp)
+
+	errHealthCheck := errors.New("health check error")
+	immuClientMock.HealthCheckF = func(context.Context) error {
+		return errHealthCheck
+	}
+	resp, err = ic.HealthCheck(nil)
+	require.Equal(t, errHealthCheck, err)
+}
---
+++ cmd/immuclient/immuc/scanners_errors_test.go
@@ -0,0 +1,118 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package immuc
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/codenotary/immudb/pkg/api/schema"
+	"github.com/codenotary/immudb/pkg/client/clienttest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestScannersErrors(t *testing.T) {
+	immuClientMock := &clienttest.ImmuClientMock{}
+	ic := &immuc{ImmuClient: immuClientMock}
+
+	// ZScan errors
+	args := []string{"set1"}
+	immuClientMock.ZScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, status.New(codes.Internal, "zscan RPC error").Err()
+	}
+	resp, err := ic.ZScan(args)
+	require.NoError(t, err)
+	require.Equal(t, " zscan RPC error", resp)
+
+	errZScan := errors.New("zscan error")
+	immuClientMock.ZScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, errZScan
+	}
+	_, err = ic.ZScan(args)
+	require.Equal(t, errZScan, err)
+
+	immuClientMock.ZScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return &schema.StructuredItemList{}, nil
+	}
+	resp, err = ic.ZScan(args)
+	require.NoError(t, err)
+	require.Equal(t, "0", resp)
+
+	// IScan errors
+	_, err = ic.IScan([]string{"X"})
+	require.Error(t, err)
+
+	_, err = ic.IScan([]string{"1", "X"})
+	require.Error(t, err)
+
+	args = []string{"1", "2"}
+	immuClientMock.IScanF = func(context.Context, uint64, uint64) (*schema.SPage, error) {
+		return nil, status.New(codes.Internal, "iscan RPC error").Err()
+	}
+	resp, err = ic.IScan(args)
+	require.NoError(t, err)
+	require.Equal(t, " iscan RPC error", resp)
+
+	errIScan := errors.New("iscan error")
+	immuClientMock.IScanF = func(context.Context, uint64, uint64) (*schema.SPage, error) {
+		return nil, errIScan
+	}
+	_, err = ic.IScan(args)
+	require.Equal(t, errIScan, err)
+
+	immuClientMock.IScanF = func(context.Context, uint64, uint64) (*schema.SPage, error) {
+		return &schema.SPage{}, nil
+	}
+	resp, err = ic.IScan(args)
+	require.NoError(t, err)
+	require.Equal(t, "0", resp)
+
+	// Scan errors
+	args = []string{"prefix1"}
+	immuClientMock.ScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, status.New(codes.Internal, "scan RPC error").Err()
+	}
+	resp, err = ic.Scan(args)
+	require.NoError(t, err)
+	require.Equal(t, " scan RPC error", resp)
+
+	errScan := errors.New("scan error")
+	immuClientMock.ScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return nil, errScan
+	}
+	_, err = ic.Scan(args)
+	require.Equal(t, errScan, err)
+
+	immuClientMock.ScanF = func(context.Context, []byte) (*schema.StructuredItemList, error) {
+		return &schema.StructuredItemList{}, nil
+	}
+	resp, err = ic.Scan(args)
+	require.NoError(t, err)
+	require.Equal(t, "0", resp)
+
+	// Count errors
+	errCount := errors.New("count error")
+	immuClientMock.CountF = func(context.Context, []byte) (*schema.ItemsCount, error) {
+		return nil, errCount
+	}
+	_, err = ic.Count(args)
+	require.Equal(t, errCount, err)
+}
