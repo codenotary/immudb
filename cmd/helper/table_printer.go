d632708e10f4f708b52bef9c95323f5d7461b471
Michele Meloni <cleaversdev@gmail.com>
2020-08-04 14:02:32 +0200

    feat(cmd/helper): add table printer
    Co-authored-by: Valentin Padurean <padurean@users.noreply.github.com>
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

---
+++ cmd/helper/table_printer.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+)
+
+func PrintTable(w io.Writer, cols []string, nbRows int, getRow func(int) []string) {
+	if nbRows == 0 {
+		return
+	}
+	nbCols := len(cols)
+	if nbCols == 0 {
+		return
+	}
+	colSep := "\t"
+
+	maxNbDigits := 0
+	tens := nbRows
+	for tens != 0 {
+		tens /= 10
+		maxNbDigits++
+	}
+	header := append([]string{strings.Repeat("#", maxNbDigits)}, cols...)
+
+	var sb strings.Builder
+	for _, th := range header {
+		for i := 0; i < len(th); i++ {
+			sb.WriteString("-")
+		}
+		sb.WriteString(colSep)
+	}
+	borderBottom := sb.String()
+	sb.Reset()
+	fmt.Fprintf(w, "%d element(s)\n", nbRows)
+
+	tw := tabwriter.NewWriter(w, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(tw, borderBottom)
+	fmt.Fprint(tw, strings.Join(header, colSep), colSep, "\n")
+	fmt.Fprintln(tw, borderBottom)
+	for i := 0; i < nbRows; i++ {
+		row := getRow(i)
+		nbRowCols := len(row)
+		for j := 0; j < nbCols; j++ {
+			if j < nbRowCols {
+				sb.WriteString(row[j])
+			}
+			sb.WriteString(colSep)
+		}
+		fmt.Fprint(tw, strconv.Itoa(i+1), colSep, sb.String(), "\n")
+		sb.Reset()
+	}
+	fmt.Fprintln(tw, borderBottom)
+	_ = tw.Flush()
+}
---
+++ cmd/helper/table_printer_test.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"github.com/codenotary/immudb/cmd/cmdtest"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestPrintTable(t *testing.T) {
+	collector := new(cmdtest.StdOutCollector)
+	collector.Start()
+	elements := make([]string, 2)
+	elements[0] = "one"
+	elements[1] = "two"
+	PrintTable(
+		os.Stdout,
+		[]string{"Database Name"},
+		len(elements),
+		func(i int) []string {
+			row := make([]string, 1)
+			row[0] = elements[i]
+			return row
+		},
+	)
+	ris, _ := collector.Stop()
+	assert.Contains(t, ris, "one")
+	assert.Contains(t, ris, "two")
+}
+
+func TestPrintTableZeroEle(t *testing.T) {
+	collector := new(cmdtest.StdOutCollector)
+	collector.Start()
+	elements := make([]string, 0)
+	PrintTable(
+		os.Stdout,
+		[]string{"Database Name"},
+		len(elements),
+		func(i int) []string {
+			row := make([]string, 1)
+			row[0] = elements[i]
+			return row
+		},
+	)
+	ris, _ := collector.Stop()
+	assert.Equal(t, "", ris)
+}
+
+func TestPrintTableZeroCol(t *testing.T) {
+	collector := new(cmdtest.StdOutCollector)
+	collector.Start()
+	elements := make([]string, 2)
+	elements[0] = "one"
+	elements[1] = "two"
+	PrintTable(
+		os.Stdout,
+		[]string{},
+		len(elements),
+		func(i int) []string {
+			row := make([]string, 1)
+			row[0] = elements[i]
+			return row
+		},
+	)
+	ris, _ := collector.Stop()
+	assert.Equal(t, "", ris)
+}
