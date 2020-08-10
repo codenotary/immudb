3311aebed9aa61cb99a0665d86568373702f099d
Michele Meloni <cleaversdev@gmail.com>
2020-08-10 09:14:00 +0200

    feat(pkg/client): add token service
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

---
+++ pkg/client/token_service_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
+
+func TestTokenSevice_setToken(t *testing.T) {
+	fn := "deleteme"
+	ts := token_service{tokenFileName: fn, hds: NewHomedirService()}
+	err := ts.SetToken("db1", "toooooken")
+	assert.Nil(t, err)
+	database, err := ts.GetDatabase()
+	assert.Nil(t, err)
+	assert.Equal(t, "db1", database)
+	token, err := ts.GetToken()
+	assert.Nil(t, err)
+	assert.Equal(t, "toooooken", token)
+	os.Remove(fn)
+}
+
+func TestToken_service_IsTokenPresent(t *testing.T) {
+	fn := "deleteme"
+	ts := token_service{tokenFileName: fn, hds: NewHomedirService()}
+	err := ts.SetToken("db1", "toooooken")
+	require.Nil(t, err)
+	ok, err := ts.IsTokenPresent()
+	assert.True(t, ok)
+}
+
+func TestToken_service_DeleteToken(t *testing.T) {
+	fn := "deleteme"
+	ts := token_service{tokenFileName: fn, hds: NewHomedirService()}
+	err := ts.SetToken("db1", "toooooken")
+	require.Nil(t, err)
+	err = ts.DeleteToken()
+	assert.Nil(t, err)
+}
