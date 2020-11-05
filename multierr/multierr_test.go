a8f6e4416648052890d7c5cb5687ccb7cddd64d6
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-11-05 16:27:41 -0300

    basic unit testing for multi error
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

---
+++ multierr/multierr_test.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package multierr
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMultiErr(t *testing.T) {
+	merr := &MultiErr{
+		Errors: []error{
+			errors.New("error1"),
+			errors.New("error2"),
+			errors.New("error3"),
+		},
+	}
+	require.Error(t, merr)
+	require.Contains(t, merr.Error(), "error1")
+	require.Contains(t, merr.Error(), "error2")
+	require.Contains(t, merr.Error(), "error3")
+}
