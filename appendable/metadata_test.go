32662b830d4573e0d421011e301038d4e8b52563
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-11-09 13:31:35 -0300

    unit testing for metadata
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

---
+++ appendable/metadata_test.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package appendable
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMedatada(t *testing.T) {
+
+	md := NewMetadata(nil)
+
+	for i := 0; i < 10; i++ {
+		md.PutInt(fmt.Sprintf("key_%d", i), i)
+	}
+
+	for i := 0; i < 10; i++ {
+		v, found := md.GetInt(fmt.Sprintf("key_%d", i))
+		require.True(t, found)
+		require.Equal(t, i, v)
+	}
+
+	md1 := NewMetadata(md.Bytes())
+
+	for i := 0; i < 10; i++ {
+		v, found := md1.GetInt(fmt.Sprintf("key_%d", i))
+		require.True(t, found)
+		require.Equal(t, i, v)
+	}
+
+}
