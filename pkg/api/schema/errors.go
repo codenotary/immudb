48be275a4f8f19d07a982ed7d3935b5850821969
Michele Meloni <cleaversdev@gmail.com>
2020-11-18 12:15:22 +0100

    fix: avoid duplicate index insertion in zAdd batch operation transaction
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- pkg/api/schema/errors.go
+++ pkg/api/schema/errors.go
@@ -9,5 +9,5 @@ var (
 	ErrUnexpectedNotStructuredValue = status.New(codes.FailedPrecondition, "unexpected not structured value encountered").Err()
 	ErrEmptySet                     = status.New(codes.InvalidArgument, "empty set").Err()
 	ErrDuplicatedKeysNotSupported   = status.New(codes.InvalidArgument, "duplicated keys are not supported in single batch transaction").Err()
-	ErrDuplicatedZAddNotSupported   = status.New(codes.InvalidArgument, "duplicated zAdd insertions are not supported in single batch transaction").Err()
+	ErrDuplicatedZAddNotSupported   = status.New(codes.InvalidArgument, "duplicated index inside zAdd insertions are not supported in single batch transaction").Err()
 )
