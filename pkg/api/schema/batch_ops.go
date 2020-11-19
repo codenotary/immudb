8eb63b77bf924f849f6fce4a8d2f761240283c74
Michele Meloni <cleaversdev@gmail.com>
2020-11-19 20:25:16 +0100

    chore: add more batchOps tests, fixing naming typo
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- pkg/api/schema/batch_ops.go
+++ pkg/api/schema/batch_ops.go
@@ -3,6 +3,8 @@ package schema
 import (
 	"bytes"
 	"crypto/sha256"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (m *BatchOps) Validate() error {
@@ -12,19 +14,27 @@ func (m *BatchOps) Validate() error {
 	mops := make(map[[32]byte]struct{}, len(m.GetOperations()))
 
 	for _, op := range m.Operations {
-		switch x := op.Operation.(type) {
-		case *BatchOp_KVs:
-			mk := sha256.Sum256(x.KVs.Key)
-			if _, ok := mops[mk]; ok {
-				return ErrDuplicatedKeysNotSupported
+		if op != nil {
+			switch x := op.Operation.(type) {
+			case *BatchOp_KVs:
+				mk := sha256.Sum256(x.KVs.Key)
+				if _, ok := mops[mk]; ok {
+					return ErrDuplicatedKeysNotSupported
+				}
+				mops[mk] = struct{}{}
+			case *BatchOp_ZOpts:
+				mk := sha256.Sum256(bytes.Join([][]byte{x.ZOpts.Set, x.ZOpts.Key, []byte(x.ZOpts.Index.String())}, nil))
+				if _, ok := mops[mk]; ok {
+					return ErrDuplicatedZAddNotSupported
+				}
+				mops[mk] = struct{}{}
+			case nil:
+				return status.New(codes.InvalidArgument, "operation is not set").Err()
+			default:
+				return status.Newf(codes.InvalidArgument, "batch operation has unexpected type %T", x).Err()
 			}
-			mops[mk] = struct{}{}
-		case *BatchOp_ZOpts:
-			mk := sha256.Sum256(bytes.Join([][]byte{x.ZOpts.Set, x.ZOpts.Key, []byte(x.ZOpts.Index.String())}, nil))
-			if _, ok := mops[mk]; ok {
-				return ErrDuplicatedZAddNotSupported
-			}
-			mops[mk] = struct{}{}
+		} else {
+			return status.New(codes.InvalidArgument, "batchOp is not set").Err()
 		}
 	}
 	return nil
