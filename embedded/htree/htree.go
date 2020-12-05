c43a20060d205d70baf22af314deae1505078f5e
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-12-05 09:45:29 -0300

    feat(embedded/htree): reusable build-at-once hash tree
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

---
+++ embedded/htree/htree.go
@@ -0,0 +1,192 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package htree
+
+import (
+	"crypto/sha256"
+	"errors"
+	"math/bits"
+)
+
+var ErrMaxWidthExceeded = errors.New("max width exceeded")
+var ErrIllegalArguments = errors.New("illegal arguments")
+var ErrIllegalState = errors.New("illegal state")
+
+const LeafPrefix = byte(0)
+const NodePrefix = byte(1)
+
+type HTree struct {
+	levels   [][][sha256.Size]byte
+	maxWidth int
+	width    int
+	root     [sha256.Size]byte
+}
+
+type InclusionProof struct {
+	i     int
+	width int
+	terms [][sha256.Size]byte
+}
+
+func New(maxWidth int) (*HTree, error) {
+	if maxWidth < 1 {
+		return nil, ErrIllegalArguments
+	}
+
+	lw := 1
+	for lw < maxWidth {
+		lw = lw << 1
+	}
+
+	height := bits.Len64(uint64(maxWidth-1)) + 1
+
+	levels := make([][][sha256.Size]byte, height)
+	for l := 0; l < height; l++ {
+		levels[l] = make([][sha256.Size]byte, lw>>l)
+	}
+
+	return &HTree{
+		levels:   levels,
+		maxWidth: maxWidth,
+	}, nil
+}
+
+func (t *HTree) BuildWith(digests [][sha256.Size]byte) error {
+	if len(digests) > t.maxWidth {
+		return ErrMaxWidthExceeded
+	}
+
+	if len(digests) == 0 {
+		return ErrIllegalArguments
+	}
+
+	for i, d := range digests {
+		leaf := [1 + sha256.Size]byte{LeafPrefix}
+		copy(leaf[1:], d[:])
+		t.levels[0][i] = sha256.Sum256(leaf[:])
+	}
+
+	l := 0
+	w := len(digests)
+
+	for w > 1 {
+		b := [1 + 2*sha256.Size]byte{NodePrefix}
+
+		wn := 0
+
+		for i := 0; i+1 < w; i += 2 {
+			copy(b[1:], t.levels[l][i][:])
+			copy(b[1+sha256.Size:], t.levels[l][i+1][:])
+			t.levels[l+1][wn] = sha256.Sum256(b[:])
+			wn++
+		}
+
+		if w%2 == 1 {
+			t.levels[l+1][wn] = t.levels[l][w-1]
+			wn++
+		}
+
+		l++
+		w = wn
+	}
+
+	t.width = len(digests)
+	t.root = t.levels[l][0]
+
+	return nil
+}
+
+func (t *HTree) Root() (root [sha256.Size]byte, err error) {
+	if t.width == 0 {
+		err = ErrIllegalState
+		return
+	}
+	return t.root, nil
+}
+
+// InclusionProof returns the shortest list of additional nodes required to compute the root
+// It's an adaption from the algorithm for proof construction at github.com/codenotary/merkletree
+func (t *HTree) InclusionProof(i int) (proof *InclusionProof, err error) {
+	if i >= t.width {
+		return nil, ErrIllegalArguments
+	}
+
+	m := i
+	n := t.width
+
+	var offset int
+	var l int
+	var r int
+
+	proof = &InclusionProof{
+		i:     i,
+		width: t.width,
+	}
+
+	for {
+		d := bits.Len(uint(n - 1))
+		k := 1 << (d - 1)
+		if m < k {
+			l, r = offset+k, offset+n-1
+			n = k
+		} else {
+			l, r = offset, offset+k-1
+			m = m - k
+			n = n - k
+			offset += k
+		}
+
+		layer := bits.Len(uint(r - l))
+		index := l / (1 << layer)
+
+		proof.terms = append([][sha256.Size]byte{t.levels[layer][index]}, proof.terms...)
+
+		if n < 1 || (n == 1 && m == 0) {
+			return
+		}
+	}
+}
+
+func VerifyInclusion(proof *InclusionProof, digest, root [sha256.Size]byte) bool {
+	if proof == nil {
+		return false
+	}
+
+	leaf := [1 + sha256.Size]byte{LeafPrefix}
+	copy(leaf[1:], digest[:])
+
+	calcRoot := sha256.Sum256(leaf[:])
+	i := proof.i
+	r := proof.width - 1
+
+	for _, t := range proof.terms {
+		b := [1 + 2*sha256.Size]byte{NodePrefix}
+
+		if i%2 == 0 && i != r {
+			copy(b[1:], calcRoot[:])
+			copy(b[1+sha256.Size:], t[:])
+		} else {
+			copy(b[1:], t[:])
+			copy(b[1+sha256.Size:], calcRoot[:])
+		}
+
+		calcRoot = sha256.Sum256(b[:])
+		i /= 2
+		r /= 2
+	}
+
+	return i == r && root == calcRoot
+}
---
+++ embedded/htree/htree_test.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2019-2020 vChain, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package htree
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTree(t *testing.T) {
+	const maxWidth = 1000
+
+	_, err := New(0)
+	require.Error(t, ErrIllegalArguments, err)
+
+	tree, err := New(maxWidth)
+	require.NoError(t, err)
+	require.NotNil(t, tree)
+
+	_, err = tree.Root()
+	require.Error(t, ErrIllegalState, err)
+
+	digests := make([][sha256.Size]byte, maxWidth)
+
+	for i := 0; i < len(digests); i++ {
+		var b [8]byte
+		binary.BigEndian.PutUint64(b[:], uint64(i))
+		digests[i] = sha256.Sum256(b[:])
+	}
+
+	err = tree.BuildWith(digests)
+	require.NoError(t, err)
+
+	root, err := tree.Root()
+	require.NoError(t, err)
+
+	for i := 0; i < len(digests); i++ {
+		proof, err := tree.InclusionProof(i)
+		require.NoError(t, err)
+		require.NotNil(t, proof)
+
+		verifies := VerifyInclusion(proof, digests[i], root)
+		require.True(t, verifies)
+
+		verifies = VerifyInclusion(proof, sha256.Sum256(digests[i][:]), root)
+		require.False(t, verifies)
+
+		verifies = VerifyInclusion(proof, digests[i], sha256.Sum256(root[:]))
+		require.False(t, verifies)
+
+		proof.terms = nil
+		verifies = VerifyInclusion(proof, digests[i], root)
+		require.False(t, verifies)
+
+		verifies = VerifyInclusion(nil, digests[i], root)
+		require.False(t, verifies)
+	}
+
+	err = tree.BuildWith(nil)
+	require.Error(t, ErrIllegalArguments, err)
+
+	err = tree.BuildWith(make([][sha256.Size]byte, maxWidth+1))
+	require.Error(t, ErrIllegalArguments, err)
+
+	_, err = tree.InclusionProof(maxWidth)
+	require.Error(t, ErrIllegalArguments, err)
+}
