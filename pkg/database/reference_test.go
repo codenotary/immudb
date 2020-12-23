4132199d00cdbcfaf8e6615f229cebbca1623532
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2020-12-23 08:15:53 -0300

    test(pkg/database): update unit testing for references
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- pkg/database/reference_test.go
+++ pkg/database/reference_test.go
@@ -18,6 +18,7 @@ package database
 
 import (
 	"crypto/sha256"
+	"strconv"
 	"testing"
 
 	"github.com/codenotary/immudb/embedded/store"
@@ -109,32 +110,39 @@ func TestStoreInvalidReferenceToReference(t *testing.T) {
 	require.Error(t, ErrReferencedKeyCannotBeAReference, err)
 }
 
-/*
 func TestStoreReferenceAsyncCommit(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	firstIndex, _ := db.Set(&schema.KeyValue{Key: []byte(`firstKey`), Value: []byte(`firstValue`)})
-	secondIndex, _ := db.Set(&schema.KeyValue{Key: []byte(`secondKey`), Value: []byte(`secondValue`)})
+	firstIndex, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`firstKey`), Value: []byte(`firstValue`)}}})
+	require.NoError(t, err)
+
+	secondIndex, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`secondKey`), Value: []byte(`secondValue`)}}})
+	require.NoError(t, err)
 
 	for n := uint64(0); n <= 64; n++ {
 		tag := []byte(strconv.FormatUint(n, 10))
 		var itemKey []byte
+		var atTx uint64
+
 		if n%2 == 0 {
 			itemKey = []byte(`firstKey`)
+			atTx = firstIndex.Id
 		} else {
 			itemKey = []byte(`secondKey`)
+			atTx = secondIndex.Id
 		}
-		refOpts := &schema.ReferenceOptions{
-			Reference: tag,
-			Key:       itemKey,
-		}
-		ref, err := db.Reference(refOpts)
-		require.NoError(t, err, "n=%d", n)
-		require.Equal(t, n+1+2, ref.GetIndex(), "n=%d", n)
-	}
 
-	db.waitForIndexing(64 + 2)
+		refOpts := &schema.ReferenceRequest{
+			Key:           tag,
+			ReferencedKey: itemKey,
+			AtTx:          atTx,
+		}
+
+		ref, err := db.SetReference(refOpts)
+		require.NoError(t, err, "n=%d", n)
+		require.Equal(t, n+1+2, ref.Id, "n=%d", n)
+	}
 
 	for n := uint64(0); n <= 64; n++ {
 		tag := []byte(strconv.FormatUint(n, 10))
@@ -144,15 +152,16 @@ func TestStoreReferenceAsyncCommit(t *testing.T) {
 		if n%2 == 0 {
 			itemKey = []byte(`firstKey`)
 			itemVal = []byte(`firstValue`)
-			index = firstIndex.GetIndex()
+			index = firstIndex.Id
 		} else {
 			itemKey = []byte(`secondKey`)
 			itemVal = []byte(`secondValue`)
-			index = secondIndex.GetIndex()
+			index = secondIndex.Id
 		}
-		item, err := db.Get(&schema.Key{Key: tag})
+
+		item, err := db.Get(&schema.KeyRequest{Key: tag, SinceTx: index})
 		require.NoError(t, err, "n=%d", n)
-		require.Equal(t, index, item.Index, "n=%d", n)
+		require.Equal(t, index, item.Tx, "n=%d", n)
 		require.Equal(t, itemVal, item.Value, "n=%d", n)
 		require.Equal(t, itemKey, item.Key, "n=%d", n)
 	}
@@ -162,58 +171,54 @@ func TestStoreMultipleReferenceOnSameKey(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	idx0, err := db.Set(&schema.KeyValue{Key: []byte(`firstKey`), Value: []byte(`firstValue`)})
-	idx1, err := db.Set(&schema.KeyValue{Key: []byte(`firstKey`), Value: []byte(`secondValue`)})
-
+	idx0, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`firstKey`), Value: []byte(`firstValue`)}}})
 	require.NoError(t, err)
 
-	refOpts1 := &schema.ReferenceOptions{
-		Reference: []byte(`myTag1`),
-		Key:       []byte(`firstKey`),
-		Index:     &schema.Index{Index: idx0.GetIndex()},
+	idx1, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`secondKey`), Value: []byte(`secondValue`)}}})
+	require.NoError(t, err)
+
+	refOpts1 := &schema.ReferenceRequest{
+		Key:           []byte(`myTag1`),
+		ReferencedKey: []byte(`firstKey`),
+		AtTx:          idx0.Id,
 	}
-	db.waitForIndexing(2)
-	reference1, err := db.Reference(refOpts1)
+
+	reference1, err := db.SetReference(refOpts1)
 	require.NoError(t, err)
-	require.Exactly(t, uint64(3), reference1.GetIndex())
+	require.Exactly(t, uint64(3), reference1.Id)
 	require.NotEmptyf(t, reference1, "Should not be empty")
 
-	refOpts2 := &schema.ReferenceOptions{
-		Reference: []byte(`myTag2`),
-		Key:       []byte(`firstKey`),
-		Index:     &schema.Index{Index: idx0.GetIndex()},
+	refOpts2 := &schema.ReferenceRequest{
+		Key:           []byte(`myTag2`),
+		ReferencedKey: []byte(`firstKey`),
+		AtTx:          idx0.Id,
 	}
-	reference2, err := db.Reference(refOpts2)
-	db.waitForIndexing(3)
+	reference2, err := db.SetReference(refOpts2)
 	require.NoError(t, err)
-	require.Exactly(t, uint64(4), reference2.GetIndex())
+	require.Exactly(t, uint64(4), reference2.Id)
 	require.NotEmptyf(t, reference2, "Should not be empty")
 
-	refOpts3 := &schema.ReferenceOptions{
-		Reference: []byte(`myTag3`),
-		Key:       []byte(`firstKey`),
-		Index:     &schema.Index{Index: idx1.GetIndex()},
+	refOpts3 := &schema.ReferenceRequest{
+		Key:           []byte(`myTag3`),
+		ReferencedKey: []byte(`secondKey`),
+		AtTx:          idx1.Id,
 	}
-	reference3, err := db.Reference(refOpts3)
-	db.waitForIndexing(4)
+	reference3, err := db.SetReference(refOpts3)
 	require.NoError(t, err)
-	require.Exactly(t, uint64(5), reference3.GetIndex())
+	require.Exactly(t, uint64(5), reference3.Id)
 	require.NotEmptyf(t, reference3, "Should not be empty")
 
-	firstTagRet, err := db.GetReference(&schema.Key{Key: []byte(`myTag1`)})
-
+	firstTagRet, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag1`), SinceTx: reference3.Id})
 	require.NoError(t, err)
 	require.NotEmptyf(t, firstTagRet, "Should not be empty")
 	require.Equal(t, []byte(`firstValue`), firstTagRet.Value, "Should have referenced item value")
 
-	secondTagRet, err := db.GetReference(&schema.Key{Key: []byte(`myTag2`)})
-
+	secondTagRet, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag2`), SinceTx: reference3.Id})
 	require.NoError(t, err)
 	require.NotEmptyf(t, secondTagRet, "Should not be empty")
 	require.Equal(t, []byte(`firstValue`), secondTagRet.Value, "Should have referenced item value")
 
-	thirdItemRet, err := db.Get(&schema.Key{Key: []byte(`myTag3`)})
-
+	thirdItemRet, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag3`), SinceTx: reference3.Id})
 	require.NoError(t, err)
 	require.NotEmptyf(t, thirdItemRet, "Should not be empty")
 	require.Equal(t, []byte(`secondValue`), thirdItemRet.Value, "Should have referenced item value")
@@ -223,101 +228,68 @@ func TestStoreIndexReference(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	idx1, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-	idx2, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item2`)})
+	idx1, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`aaa`), Value: []byte(`item1`)}}})
+	require.NoError(t, err)
 
-	db.waitForIndexing(2)
+	idx2, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`aaa`), Value: []byte(`item2`)}}})
+	require.NoError(t, err)
 
-	db.Reference(&schema.ReferenceOptions{Key: []byte(`aaa`), Reference: []byte(`myTag1`), Index: &schema.Index{Index: idx1.GetIndex()}})
-	db.Reference(&schema.ReferenceOptions{Key: []byte(`aaa`), Reference: []byte(`myTag2`), Index: &schema.Index{Index: idx2.GetIndex()}})
+	ref, err := db.SetReference(&schema.ReferenceRequest{ReferencedKey: []byte(`aaa`), Key: []byte(`myTag1`), AtTx: idx1.Id})
+	require.NoError(t, err)
 
-	db.waitForIndexing(4)
+	ref, err = db.SetReference(&schema.ReferenceRequest{ReferencedKey: []byte(`aaa`), Key: []byte(`myTag2`), AtTx: idx2.Id})
+	require.NoError(t, err)
 
-	tag1, err := db.GetReference(&schema.Key{Key: []byte(`myTag1`)})
+	tag1, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag1`), SinceTx: ref.Id})
 	require.NoError(t, err)
 	require.Equal(t, []byte(`aaa`), tag1.Key)
 	require.Equal(t, []byte(`item1`), tag1.Value)
 
-	tag2, err := db.Get(&schema.Key{Key: []byte(`myTag2`)})
+	tag2, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag2`), SinceTx: ref.Id})
 	require.NoError(t, err)
 	require.Equal(t, []byte(`aaa`), tag2.Key)
 	require.Equal(t, []byte(`item2`), tag2.Value)
-
 }
 
 func TestStoreReferenceKeyNotProvided(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
-	_, err := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag1`), Index: &schema.Index{Index: 123}})
-	require.Equal(t, err, ErrReferenceKeyMissing)
+	_, err := db.SetReference(&schema.ReferenceRequest{Key: []byte(`myTag1`), AtTx: 123})
+	require.Error(t, store.ErrIllegalArguments, err)
 }
 
 func TestStore_GetOnReferenceOnSameKeyReturnsAlwaysLastValue(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	idx1, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-	idx2, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item2`)})
+	idx1, err := db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`aaa`), Value: []byte(`item1`)}}})
+	require.NoError(t, err)
 
-	db.waitForIndexing(idx2.GetIndex())
+	_, err = db.Set(&schema.SetRequest{KVs: []*schema.KeyValue{{Key: []byte(`aaa`), Value: []byte(`item2`)}}})
+	require.NoError(t, err)
 
-	ref, _ := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag1`), Key: []byte(`aaa`), Index: &schema.Index{Index: idx1.GetIndex()}})
+	_, err = db.SetReference(&schema.ReferenceRequest{Key: []byte(`myTag1`), ReferencedKey: []byte(`aaa`)})
+	require.NoError(t, err)
 
-	db.waitForIndexing(ref.GetIndex())
+	ref, err := db.SetReference(&schema.ReferenceRequest{Key: []byte(`myTag2`), ReferencedKey: []byte(`aaa`), AtTx: idx1.Id})
+	require.NoError(t, err)
 
-	ref2, _ := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag2`), Key: []byte(`aaa`), Index: &schema.Index{Index: idx2.GetIndex()}})
-
-	db.waitForIndexing(ref2.GetIndex())
-
-	tag2, err := db.Get(&schema.Key{Key: []byte(`myTag2`)})
+	tag2, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag2`), SinceTx: ref.Id})
 	require.NoError(t, err)
 	require.Equal(t, []byte(`aaa`), tag2.Key)
-	require.Equal(t, []byte(`item2`), tag2.Value)
+	require.Equal(t, []byte(`item1`), tag2.Value)
 
-	tag1b, err := db.Get(&schema.Key{Key: []byte(`myTag1`)})
+	tag1b, err := db.Get(&schema.KeyRequest{Key: []byte(`myTag1`), SinceTx: ref.Id})
 	require.NoError(t, err)
 	require.Equal(t, []byte(`aaa`), tag1b.Key)
 	require.Equal(t, []byte(`item2`), tag1b.Value)
-}
-
-func TestStore_GetOnReferenceOnSameKeyMixReturnsAlwaysLastValue(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	idx1, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-	idx2, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item2`)})
-	idx3, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item3`)})
-
-	db.waitForIndexing(idx3.GetIndex())
-
-	ref1, _ := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag1`), Key: []byte(`aaa`), Index: &schema.Index{Index: idx1.GetIndex()}})
-	db.waitForIndexing(ref1.GetIndex())
-	ref2, _ := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag2`), Key: []byte(`aaa`), Index: &schema.Index{Index: idx2.GetIndex()}})
-	db.waitForIndexing(ref2.GetIndex())
-	ref3, _ := db.Reference(&schema.ReferenceOptions{Reference: []byte(`myTag3`), Key: []byte(`aaa`)})
-	db.waitForIndexing(ref3.GetIndex())
-
-	tag2, err := db.Get(&schema.Key{Key: []byte(`myTag2`)})
-	require.NoError(t, err)
-	require.Equal(t, []byte(`aaa`), tag2.Key)
-	require.Equal(t, []byte(`item3`), tag2.Value)
-
-	tag1, err := db.Get(&schema.Key{Key: []byte(`myTag1`)})
-	require.NoError(t, err)
-	require.Equal(t, []byte(`aaa`), tag1.Key)
-	require.Equal(t, []byte(`item3`), tag1.Value)
-
-	tag3, err := db.Get(&schema.Key{Key: []byte(`myTag3`)})
-	require.NoError(t, err)
-	require.Equal(t, []byte(`aaa`), tag3.Key)
-	require.Equal(t, []byte(`item3`), tag3.Value)
 }
 
 func TestStore_ReferenceIllegalArgument(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	_, err := db.Reference(nil)
+	_, err := db.SetReference(nil)
 	require.Equal(t, err, store.ErrIllegalArguments)
 }
 
@@ -325,60 +297,6 @@ func TestStore_ReferencedItemNotFound(t *testing.T) {
 	db, closer := makeDb()
 	defer closer()
 
-	_, err := db.Reference(&schema.ReferenceOptions{Key: []byte(`aaa`), Reference: []byte(`notExists`)})
-	require.Equal(t, err, errors.New("unexpected error key not found during Reference"))
+	_, err := db.SetReference(&schema.ReferenceRequest{ReferencedKey: []byte(`aaa`), Key: []byte(`notExists`)})
+	require.Error(t, store.ErrKeyNotFound, err)
 }
-
-func TestStore_GetReferenceIllegalArgument(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	_, err := db.GetReference(nil)
-	require.Equal(t, err, store.ErrIllegalArguments)
-}
-func TestStore_GetReferencedItemNotFound(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	_, err := db.GetReference(&schema.Key{Key: []byte(`aaa`)})
-	require.Equal(t, err, errors.New("key not found"))
-}
-
-func TestStore_GetReferencedNoReferenceProvided(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	idx1, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-
-	db.waitForIndexing(idx1.GetIndex())
-
-	_, err := db.GetReference(&schema.Key{Key: []byte(`aaa`)})
-	require.Equal(t, err, ErrNoReferenceProvided)
-}
-
-func TestStore_getReferenceValErrIndexKeyMismatch(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	_, _ = db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-	idx2, _ := db.Set(&schema.KeyValue{Key: []byte(`bbb`), Value: []byte(`item2`)})
-
-	db.waitForIndexing(idx2.GetIndex())
-
-	_, err := db.getReferenceVal(&schema.ReferenceOptions{Key: []byte(`bbb`), Index: &schema.Index{Index: 1}, Reference: []byte(`myTag`)}, false)
-
-	require.Equal(t, err, ErrIndexKeyMismatch)
-}
-
-func TestStore_getReferenceValEOF(t *testing.T) {
-	db, closer := makeDb()
-	defer closer()
-
-	idx1, _ := db.Set(&schema.KeyValue{Key: []byte(`aaa`), Value: []byte(`item1`)})
-
-	db.waitForIndexing(idx1.GetIndex())
-
-	_, err := db.getReferenceVal(&schema.ReferenceOptions{Key: []byte(`aaa`), Index: &schema.Index{Index: 99999999}, Reference: []byte(`myTag`)}, false)
-
-	require.Equal(t, err, errors.New("EOF"))
-}*/
