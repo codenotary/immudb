64071be0e370b6f0068b1e8b1dcd8b84a5d5c8a4
Michele Meloni <cleaversdev@gmail.com>
2021-01-05 21:54:47 +0100

    test(cmd/immudb/command/service): inncrease code coverage
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- cmd/immudb/command/service/servicetest/sservice.go
+++ cmd/immudb/command/service/servicetest/sservice.go
@@ -17,49 +17,107 @@ limitations under the License.
 package servicetest
 
 import (
+	"github.com/codenotary/immudb/cmd/sservice"
 	"github.com/spf13/cobra"
 	"github.com/takama/daemon"
 )
 
-type Sservicemock struct{}
+func NewSservicemock() *Sservicemock {
+	ss := &Sservicemock{}
+	ss.NewDaemonF = func(serviceName, description string, dependencies ...string) (d daemon.Daemon, err error) {
+		return NewDaemonMock(), nil
+	}
 
-func (ss Sservicemock) NewDaemon(name, description string, dependencies ...string) (d daemon.Daemon, err error) {
-	return daemonmock{}, nil
+	ss.IsAdminF = func() (bool, error) {
+		return true, nil
+	}
+	ss.InstallSetupF = func(serviceName string, cmd *cobra.Command) (err error) {
+		return nil
+	}
+	ss.UninstallSetupF = func(serviceName string) (err error) {
+		return nil
+	}
+	ss.EraseDataF = func(serviceName string) (err error) {
+		return nil
+	}
+	ss.IsRunningF = func(status string) bool {
+		return false
+	}
+	ss.GetDefaultConfigPathF = func(serviceName string) (string, error) {
+		return "", nil
+	}
+
+	ss.ReadConfigF = func(serviceName string) (err error) {
+		return nil
+	}
+	ss.InstallConfigF = func(serviceName string) (err error) {
+		return nil
+	}
+	ss.CopyExecInOsDefaultF = func(execPath string) (newExecPath string, err error) {
+		return "", nil
+	}
+	ss.GetDefaultExecPathF = func(serviceName string) (string, error) {
+		return "", nil
+	}
+	ss.UninstallExecutablesF = func(serviceName string) error {
+		return nil
+	}
+	return ss
 }
 
-func (ss Sservicemock) IsAdmin() (bool, error) {
-	return true, nil
-}
-func (ss Sservicemock) InstallSetup(serviceName string, cmd *cobra.Command) (err error) {
-	return nil
-}
-func (ss Sservicemock) UninstallSetup(serviceName string) (err error) {
-	return nil
-}
-func (ss Sservicemock) EraseData(serviceName string) (err error) {
-	return nil
-}
-func (ss Sservicemock) IsRunning(status string) bool {
-	return false
-}
-func (ss Sservicemock) GetDefaultConfigPath(serviceName string) (string, error) {
-	return "", nil
+type Sservicemock struct {
+	sservice.Sservice
+	NewDaemonF            func(serviceName, description string, dependencies ...string) (d daemon.Daemon, err error)
+	IsAdminF              func() (bool, error)
+	InstallSetupF         func(serviceName string, cmd *cobra.Command) (err error)
+	UninstallSetupF       func(serviceName string) (err error)
+	EraseDataF            func(serviceName string) (err error)
+	IsRunningF            func(status string) bool
+	GetDefaultConfigPathF func(serviceName string) (string, error)
+	ReadConfigF           func(serviceName string) (err error)
+	InstallConfigF        func(serviceName string) (err error)
+	CopyExecInOsDefaultF  func(execPath string) (newExecPath string, err error)
+	GetDefaultExecPathF   func(serviceName string) (string, error)
+	UninstallExecutablesF func(serviceName string) error
 }
 
-func (ss Sservicemock) ReadConfig(serviceName string) (err error) {
-	return nil
+func (ss *Sservicemock) NewDaemon(name, description string, dependencies ...string) (d daemon.Daemon, err error) {
+	return ss.NewDaemonF(name, description, dependencies...)
 }
-func (ss Sservicemock) InstallConfig(serviceName string) (err error) {
-	return nil
+
+func (ss *Sservicemock) IsAdmin() (bool, error) {
+	return ss.IsAdminF()
 }
-func (ss Sservicemock) CopyExecInOsDefault(execPath string) (newExecPath string, err error) {
-	return "", nil
+func (ss *Sservicemock) InstallSetup(serviceName string, cmd *cobra.Command) (err error) {
+	return ss.InstallSetupF(serviceName, cmd)
 }
-func (ss Sservicemock) GetDefaultExecPath(serviceName string) (string, error) {
-	return "", nil
+func (ss *Sservicemock) UninstallSetup(serviceName string) (err error) {
+	return ss.UninstallSetupF(serviceName)
 }
-func (ss Sservicemock) UninstallExecutables(serviceName string) error {
-	return nil
+func (ss *Sservicemock) EraseData(serviceName string) (err error) {
+	return ss.EraseDataF(serviceName)
+}
+func (ss *Sservicemock) IsRunning(status string) bool {
+	return ss.IsRunningF(status)
+}
+func (ss *Sservicemock) GetDefaultConfigPath(serviceName string) (string, error) {
+	return ss.GetDefaultConfigPathF(serviceName)
+}
+
+func (ss *Sservicemock) ReadConfig(serviceName string) (err error) {
+	return ss.ReadConfigF(serviceName)
+}
+func (ss *Sservicemock) InstallConfig(serviceName string) (err error) {
+	return ss.InstallConfigF(serviceName)
+}
+func (ss *Sservicemock) CopyExecInOsDefault(execPath string) (newExecPath string, err error) {
+	return ss.CopyExecInOsDefaultF(execPath)
+}
+func (ss *Sservicemock) GetDefaultExecPath(serviceName string) (string, error) {
+	return ss.CopyExecInOsDefaultF(serviceName)
+}
+func (ss *Sservicemock) UninstallExecutables(serviceName string) error {
+	return ss.UninstallExecutablesF(serviceName)
 }
 
 type SservicePermissionsMock struct{}
