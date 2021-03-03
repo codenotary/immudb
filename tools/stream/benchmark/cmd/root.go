237836e60925c1ff22d80b75d7a3575c22fdc6d9
Michele Meloni <cleaversdev@gmail.com>
2021-03-03 19:37:59 +0100

    chore(tools): fix copyright
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- tools/stream/benchmark/cmd/root.go
+++ tools/stream/benchmark/cmd/root.go
@@ -1,11 +1,11 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright 2021 CodeNotary, Inc. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package cmd
 
 import (
@@ -30,7 +31,7 @@ var cfgFile string
 var rootCmd = &cobra.Command{
 	Use:   "streamb",
 	Short: "Immudb stream benchmark",
-	Long: `Immudb stream benchmark.`,
+	Long:  `Immudb stream benchmark.`,
 
 	RunE: func(cmd *cobra.Command, args []string) error {
 		iterations, err := cmd.Flags().GetInt("iterations")
