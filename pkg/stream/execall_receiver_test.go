1bb7883f7d1232b69ad3615fcd2212e2eb6004e4
Michele Meloni <cleaversdev@gmail.com>
2021-03-08 18:53:44 +0100

    test(pkg/stream): add exec all unit tests
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

---
+++ pkg/stream/execall_receiver_test.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2021 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package stream
+
+import (
+	"bytes"
+	"github.com/codenotary/immudb/pkg/stream/streamtest"
+	"github.com/stretchr/testify/require"
+	"io"
+	"testing"
+)
+
+func TestNewExecAllStreamReceiver(t *testing.T) {
+	r := bytes.NewBuffer([]byte{})
+	esr := NewExecAllStreamReceiver(r, 4096)
+	require.IsType(t, new(execAllStreamReceiver), esr)
+}
+
+func TestExecAllStreamReceiver_Next(t *testing.T) {
+	me := []*streamtest.MsgError{
+		{M: []byte{1, 1, 1}, E: io.EOF},
+		{M: []byte{1, 1, 1}, E: io.EOF},
+	}
+	r := streamtest.DefaultMsgReceiverMock(me)
+	esr := NewExecAllStreamReceiver(r, 4096)
+	op, err := esr.Next()
+	require.NoError(t, err)
+	require.NotNil(t, op)
+}
