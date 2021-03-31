dc435e5b1e5cd04befac4aa28ef7a1cddd2a3b49
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2021-03-31 17:34:32 -0300

    feat(pkg/server): initial integration of sql engine
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

---
+++ pkg/server/sql.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2021 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package server
+
+import (
+	"context"
+
+	"github.com/codenotary/immudb/pkg/api/schema"
+)
+
+func (s *ImmuServer) SQLExec(ctx context.Context, req *schema.SQLExecRequest) (*schema.SQLExecResult, error) {
+	ind, err := s.getDbIndexFromCtx(ctx, "SQLExec")
+	if err != nil {
+		return nil, err
+	}
+
+	return s.dbList.GetByIndex(ind).SQLExec(req)
+}
+
+func (s *ImmuServer) SQLQuery(ctx context.Context, req *schema.SQLQueryRequest) (*schema.SQLQueryResult, error) {
+	ind, err := s.getDbIndexFromCtx(ctx, "SQLQuery")
+	if err != nil {
+		return nil, err
+	}
+
+	return s.dbList.GetByIndex(ind).SQLQuery(req)
+}
