ec8fca0331068c639fccb28eaa99774578c2fa60
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2021-03-31 17:34:32 -0300

    feat(pkg/server): initial integration of sql engine
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package server
 
 import (
@@ -23,7 +22,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/codenotary/immudb/pkg/stream"
 	"io/ioutil"
 	"log"
 	"math"
@@ -36,6 +34,8 @@ import (
 	"syscall"
 	"time"
 	"unicode"
+
+	"github.com/codenotary/immudb/pkg/stream"
 
 	"github.com/codenotary/immudb/pkg/database"
 
