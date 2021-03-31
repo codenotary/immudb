ddf526905ebb6609122122c47319b392f271dbba
Duncan Mac-Vicar P <duncan@codenotary.com>
2021-03-31 16:56:14 +0200

    chore(embedded/appendable) cover ReadAt EOF case

--- embedded/appendable/singleapp/single_app_test.go
+++ embedded/appendable/singleapp/single_app_test.go
@@ -82,6 +82,11 @@ func TestSingleApp(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, []byte{7, 8, 9, 10}, bs)
 
+	n , err = a.ReadAt(bs, 1000)
+	require.Error(t, err)
+	require.Equal(t, n, 0)
+	require.Contains(t, err.Error(), "EOF")
+
 	err = a.Sync()
 	require.NoError(t, err)
 
