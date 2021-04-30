09f308c11413b5a6be116b2579be265e9fccf1d2
Duncan Mac-Vicar P <duncan@codenotary.com>
2021-04-30 18:11:15 +0200

    chore(pkg/api/schema): Handle tools via modules (#726)

---
+++ tools.go
@@ -0,0 +1,12 @@
+// +build tools
+package schema
+
+import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "google.golang.org/grpc"
+	_ "github.com/golang/protobuf/proto"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
+
+)
