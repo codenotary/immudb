7232ba2bc34934292200398c83ee3a6c3e89ee58
Michele Meloni <cleaversdev@gmail.com>
2021-05-06 15:45:26 +0200

    chore(pkg/pgsql): handle empty response and command complete message
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- pkg/pgsql/server/bmessages/empty_query_response.go
+++ pkg/pgsql/server/bmessages/empty_query_response.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+	http//www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,11 +21,9 @@ import (
 	"encoding/binary"
 )
 
-func CommandComplete(msg []byte) []byte {
-	messageType := []byte(`C`)
-	msg = bytes.Join([][]byte{msg, {0}}, nil)
-	selfMessageLength := make([]byte, 4)
-	binary.BigEndian.PutUint32(selfMessageLength, uint32(len(msg)+4))
-
-	return bytes.Join([][]byte{messageType, selfMessageLength, msg}, nil)
+func EmptyQueryResponse() []byte {
+	messageType := []byte(`I`)
+	message := make([]byte, 4)
+	binary.BigEndian.PutUint32(message, uint32(4))
+	return bytes.Join([][]byte{messageType, message}, nil)
 }
