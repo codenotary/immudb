66a5f81214d07a92969dec22263eabe8e4f83cf7
Michele Meloni <cleaversdev@gmail.com>
2021-05-14 07:37:10 +0200

    test(pkg/pgsql/server/bmessages): add error response tests
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

---
+++ pkg/pgsql/server/bmessages/error_response_test.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2021 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package bmessages
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestErrorResponse(t *testing.T) {
+	er := ErrorResponse(Severity("severity"),
+		Code("test"),
+		Message("test"),
+		Hint("test"),
+		SeverityNotLoc("test"),
+		Detail("test"),
+		Position("test"),
+		InternalPosition("test"),
+		InternalQuery("test"),
+		Where("test"),
+		SchemaName("test"),
+		TableName("test"),
+		ColumnName("test"),
+		DataTypeName("test"),
+		ConstraintName("test"),
+		File("test"),
+		Line("test"),
+		Routine("test"),
+	)
+	require.NotNil(t, er)
+}
