82a4186898785b3631478d576d5abb2319467da7
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2021-06-09 12:02:49 -0300

    chore(pkg/database): sql operations after catalog is created
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -89,7 +89,8 @@ type DB interface {
 type db struct {
 	st *store.ImmuStore
 
-	sqlEngine *sql.Engine
+	sqlEngine  *sql.Engine
+	dbSelected bool
 
 	tx1, tx2 *store.Tx
 	mutex    sync.RWMutex
@@ -135,6 +136,8 @@ func OpenDb(op *DbOptions, systemDB DB, log logger.Logger) (DB, error) {
 		return nil, logErr(dbi.Logger, "Unable to open store: %s", err)
 	}
 
+	dbi.dbSelected = err == nil
+
 	if err == sql.ErrDatabaseDoesNotExist && !op.replica {
 		dbi.Logger.Infof("Migrating catalog from systemdb to %s...", dbDir)
 
@@ -172,6 +175,8 @@ func OpenDb(op *DbOptions, systemDB DB, log logger.Logger) (DB, error) {
 		if err != nil {
 			return nil, logErr(dbi.Logger, "Unable to open store: %s", err)
 		}
+
+		dbi.dbSelected = true
 	}
 
 	return dbi, nil
