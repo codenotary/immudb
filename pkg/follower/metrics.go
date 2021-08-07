2ff77b4e88bcc652d3918b444d58aaf6625d30bc
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2021-08-07 10:59:23 -0300

    feat(pkg/follower): follower replication
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

---
+++ pkg/follower/metrics.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2021 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package follower
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+
+	// ---- Fetching committed transactions ---------------------------------------
+
+	metricsFollowerStreammingEvents = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "immudb_follower_streamming_events",
+		Help: "immudb follower streamming event counters",
+	}, []string{"event"})
+
+	metricsTxStreamStarted   = metricsFollowerStreammingEvents.WithLabelValues("started")
+	metricsTxStreamFinished  = metricsFollowerStreammingEvents.WithLabelValues("finished")
+	metricsTxStreamFailed    = metricsFollowerStreammingEvents.WithLabelValues("failed")
+	metricsTxStreamCancelled = metricsFollowerStreammingEvents.WithLabelValues("cancelled")
+	metricsTxStreamRetried   = metricsFollowerStreammingEvents.WithLabelValues("retried")
+	metricsTxStreamSucceeded = metricsFollowerStreammingEvents.WithLabelValues("succeeded")
+)
