16dbf82c9aa7400ba7560fb94295551cfdd07b57
Michele Meloni <cleaversdev@gmail.com>
2021-09-17 12:43:12 +0200

    chore(pkg/stdlib): increase code coverage and fix blob results scan
    Signed-off-by: Michele Meloni <cleaversdev@gmail.com>

--- pkg/stdlib/sql_test.go
+++ pkg/stdlib/sql_test.go
@@ -86,6 +86,70 @@ func TestOpenDB(t *testing.T) {
 	require.Equal(t, uint64(2), id)
 	require.Equal(t, "immu2", name)
 
+	rowsw, err := db.QueryContext(context.TODO(), fmt.Sprintf("SELECT * FROM %s WHERE id = 2", table))
+	require.NoError(t, err)
+	rowsw.Next()
+	err = rowsw.Scan(&id, &name)
+	if err != nil {
+		require.NoError(t, err)
+	}
+	require.Equal(t, uint64(2), id)
+	require.Equal(t, "immu2", name)
+
+	require.False(t, rowsw.Next())
+}
+
+func TestQueryCapabilities(t *testing.T) {
+	options := server.DefaultOptions().WithAuth(true)
+	bs := servertest.NewBufconnServer(options)
+
+	bs.Start()
+	defer bs.Stop()
+
+	defer os.RemoveAll(options.Dir)
+	defer os.Remove(".state-")
+
+	opts := client.DefaultOptions()
+	opts.Username = "immudb"
+	opts.Password = "immudb"
+	opts.Database = "defaultdb"
+
+	opts.WithDialOptions(&[]grpc.DialOption{grpc.WithContextDialer(bs.Dialer), grpc.WithInsecure()})
+
+	db := OpenDB(opts)
+	defer db.Close()
+
+	table := getRandomTableName()
+	result, err := db.ExecContext(context.TODO(), fmt.Sprintf("CREATE TABLE %s (id INTEGER, amount INTEGER, total INTEGER, title VARCHAR, content BLOB, isPresent BOOLEAN, PRIMARY KEY id)", table))
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	binaryContent := []byte("my blob content1")
+	_, err = db.ExecContext(context.TODO(), fmt.Sprintf("INSERT INTO %s (id, amount, total, title, content, isPresent) VALUES (?, ?, ?, ?, ?, ?)", table), 1, 1000, 6000, "title 1", binaryContent, true)
+	require.NoError(t, err)
+	binaryContent2 := []byte("my blob content2")
+	_, err = db.ExecContext(context.TODO(), fmt.Sprintf("INSERT INTO %s (id, amount, total, title, content, isPresent) VALUES (?, ?, ?, ?, ?, ?)", table), 2, 2000, 12000, "title 2", binaryContent2, true)
+	require.NoError(t, err)
+
+	var id int64
+	var amount int64
+	var title string
+	var isPresent bool
+	var content []byte
+
+	rows, err := db.QueryContext(context.Background(), fmt.Sprintf("SELECT id, amount, title, content, isPresent FROM %s where isPresent=? and id=? and amount=? and total=? and title=?", table), true, 1, 1000, 6000, "title 1")
+	defer rows.Close()
+
+	require.NoError(t, err)
+	rows.Next()
+	err = rows.Scan(&id, &amount, &title, &content, &isPresent)
+	require.NoError(t, err)
+
+	require.NoError(t, err)
+	require.Equal(t, int64(1), id)
+	require.Equal(t, int64(1000), amount)
+	require.Equal(t, "title 1", title)
+	require.Equal(t, binaryContent, content)
+	require.Equal(t, true, isPresent)
 }
 
 func TestDriverConnector_ConnectErr(t *testing.T) {
