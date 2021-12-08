35456ad62c91757c9a52b60892071065a783e8e1
Bartłomiej Święcki <bart@codenotary.com>
2021-12-08 09:17:45 +0100

    fix(database): Internal consistency check on data reads
    Signed-off-by: Bartłomiej Święcki <bart@codenotary.com>

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -411,8 +411,21 @@ func (d *db) getAt(key []byte, atTx uint64, resolved int, index store.KeyIndex, 
 		}
 	}
 
+	if len(val) < 1 {
+		return nil, fmt.Errorf(
+			"%w: internal value consistency error - missing value prefix",
+			store.ErrCorruptedData,
+		)
+	}
+
 	//Reference lookup
 	if val[0] == ReferenceValuePrefix {
+		if len(val) < 1+8 {
+			return nil, fmt.Errorf(
+				"%w: internal value consistency error - invalid reference",
+				store.ErrCorruptedData,
+			)
+		}
 		if resolved == MaxKeyResolutionLimit {
 			return nil, ErrMaxKeyResolutionLimitReached
 		}
