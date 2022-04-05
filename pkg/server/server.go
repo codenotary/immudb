1a9986a36ae02a52d99e9c0fa6ee11a565111a32
Bartłomiej Święcki <bart@codenotary.com>
2022-04-05 13:22:42 +0200

    fix(pkg/server): Use buffered channel for catching OS signals
    Issue detected by go vet
    
    Signed-off-by: Bartłomiej Święcki <bart@codenotary.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -711,7 +711,7 @@ func (s *ImmuServer) Health(ctx context.Context, _ *empty.Empty) (*schema.Health
 }
 
 func (s *ImmuServer) installShutdownHandler() {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 	go func() {
