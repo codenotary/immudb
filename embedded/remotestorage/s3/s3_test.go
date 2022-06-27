9d000b1f1380634a80fb24c5eebec8a8716f5ca0
Bartłomiej Święcki <bart@codenotary.com>
2022-06-27 12:54:43 +0200

    test(embedded/remotestorage): Increase testing coverage
    * Cover more paths when server responds with an error
    * Test listiung entries
    
    Signed-off-by: Bartłomiej Święcki <bart@codenotary.com>

--- embedded/remotestorage/s3/s3_test.go
+++ embedded/remotestorage/s3/s3_test.go
@@ -18,9 +18,12 @@ package s3
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 	"time"
 
@@ -161,6 +164,54 @@ func TestCornerCases(t *testing.T) {
 		require.ErrorIs(t, err, ErrInvalidArguments)
 		require.ErrorIs(t, err, ErrInvalidArgumentsOffsSize)
 	})
+
+	t.Run("invalid list path", func(t *testing.T) {
+		s, err := Open(
+			"https://localhost:9000",
+			"minioadmin",
+			"minioadmin",
+			"bucket",
+			"",
+			"",
+		)
+		require.NoError(t, err)
+
+		_, _, err = s.ListEntries(context.Background(), "prefix-no-slash")
+		require.ErrorIs(t, err, ErrInvalidArguments)
+
+		_, _, err = s.ListEntries(context.Background(), "prefix-double-slash//")
+		require.ErrorIs(t, err, ErrInvalidArguments)
+	})
+
+	t.Run("invalid http status code from the server", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "test error", http.StatusInternalServerError)
+		}))
+		defer ts.Close()
+
+		s, err := Open(ts.URL, "", "", "bucket", "", "")
+		require.NoError(t, err)
+
+		ctx := context.Background()
+
+		_, err = s.Get(ctx, "object1", 0, -1)
+		require.ErrorIs(t, err, ErrInvalidResponse)
+	})
+
+	t.Run("invalid upload file path", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			require.Fail(t, "Should not call the server")
+		}))
+		defer ts.Close()
+
+		s, err := Open(ts.URL, "", "", "bucket", "", "")
+		require.NoError(t, err)
+
+		ctx := context.Background()
+
+		err = s.Put(ctx, "object1", "/invalid/file/path/that/does/not/exist")
+		require.IsType(t, &fs.PathError{}, err)
+	})
 }
 
 func TestSignatureV4(t *testing.T) {
@@ -236,6 +287,14 @@ func TestHandlingRedirects(t *testing.T) {
 			require.Equal(t, "GET", r.Method)
 			http.Redirect(w, r, "h**p://invalid", http.StatusTemporaryRedirect)
 
+		case "/bucket/object7":
+			require.Equal(t, "PUT", r.Method)
+			http.Redirect(w, r, "h**p://invalid", http.StatusSeeOther)
+
+		case "/bucket/object8":
+			require.Equal(t, "PUT", r.Method)
+			http.Redirect(w, r, "h**p://invalid", http.StatusTemporaryRedirect)
+
 		default:
 			require.Fail(t, "Unknown request")
 		}
@@ -269,14 +328,276 @@ func TestHandlingRedirects(t *testing.T) {
 
 	t.Run("error getting 303 redirect", func(t *testing.T) {
 		_, err := s.Get(ctx, "object5", 0, -1)
-		require.Error(t, err)
+		require.ErrorIs(t, err, ErrInvalidResponse)
 		require.Contains(t, err.Error(), "failed to parse Location header")
 	})
 
 	t.Run("error getting 307 redirect", func(t *testing.T) {
 		_, err := s.Get(ctx, "object6", 0, -1)
-		require.Error(t, err)
+		require.ErrorIs(t, err, ErrInvalidResponse)
 		require.Contains(t, err.Error(), "failed to parse Location header")
+	})
+
+	t.Run("error getting 303 redirect while PUT request", func(t *testing.T) {
+		fl, err := ioutil.TempFile("", "")
+		require.NoError(t, err)
+		fmt.Fprintf(fl, "Hello world")
+		fl.Close()
+		defer os.Remove(fl.Name())
+
+		err = s.Put(ctx, "object7", fl.Name())
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.Contains(t, err.Error(), "failed to parse Location header")
+	})
+
+	t.Run("error getting 307 redirect", func(t *testing.T) {
+		fl, err := ioutil.TempFile("", "")
+		require.NoError(t, err)
+		fmt.Fprintf(fl, "Hello world")
+		fl.Close()
+		defer os.Remove(fl.Name())
+
+		err = s.Put(ctx, "object8", fl.Name())
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.Contains(t, err.Error(), "failed to parse Location header")
+	})
+}
+
+func TestListEntries(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "/bucket/", r.URL.Path)
+
+		switch r.URL.Query().Get("prefix") {
+		case "path1/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>path1/photos1/</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>path1/photos2/</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path2/":
+			w.Write([]byte(`
+				< this is not a valid xml >
+			`))
+
+		case "path3/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>path3/photos1/</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>path3/photos2/</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path4/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>path4/photos2/</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>path4/photos1/</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path5/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>invalid-prefix/photos1/</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>invalid-prefix/photos2/</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path6/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>path6/photos1</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>path6/photos2</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path7/":
+			w.Write([]byte(`
+				<?xml version="1.0" encoding="UTF-8"?>
+				<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+					<IsTruncated>false</IsTruncated>
+					<Contents>
+						<Key>ExampleObject1.txt</Key>
+					</Contents>
+					<Contents>
+						<Key>ExampleObject2.txt</Key>
+					</Contents>
+					<CommonPrefixes>
+						<Prefix>path7/photos1/</Prefix>
+					</CommonPrefixes>
+					<CommonPrefixes>
+						<Prefix>path7/../photos2/</Prefix>
+					</CommonPrefixes>
+				</ListBucketResult>
+			`))
+
+		case "path8/":
+			switch r.URL.Query().Get("continuation-token") {
+			case "":
+				w.Write([]byte(`
+					<?xml version="1.0" encoding="UTF-8"?>
+					<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+						<IsTruncated>true</IsTruncated>
+						<NextContinuationToken>cont-token</NextContinuationToken>
+						<Contents>
+							<Key>ExampleObject1.txt</Key>
+						</Contents>
+						<Contents>
+							<Key>ExampleObject2.txt</Key>
+						</Contents>
+						<CommonPrefixes>
+							<Prefix>path8/photos1/</Prefix>
+						</CommonPrefixes>
+						<CommonPrefixes>
+							<Prefix>path8/photos2/</Prefix>
+						</CommonPrefixes>
+					</ListBucketResult>
+				`))
+			case "cont-token":
+				w.Write([]byte(`
+					<?xml version="1.0" encoding="UTF-8"?>
+					<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+						<IsTruncated>false</IsTruncated>
+						<Contents>
+							<Key>ExampleObject3.txt</Key>
+						</Contents>
+						<Contents>
+							<Key>ExampleObject4.txt</Key>
+						</Contents>
+						<CommonPrefixes>
+							<Prefix>path8/photos3/</Prefix>
+						</CommonPrefixes>
+						<CommonPrefixes>
+							<Prefix>path8/photos4/</Prefix>
+						</CommonPrefixes>
+					</ListBucketResult>
+				`))
+			default:
+				require.Fail(t, "invalid continuation token")
+			}
+
+		default:
+			require.Fail(t, "Invalid request")
+		}
+
+	}))
+	defer ts.Close()
+
+	s, err := Open(ts.URL, "", "", "bucket", "", "")
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	t.Run("correct response", func(t *testing.T) {
+		entries, subPaths, err := s.ListEntries(ctx, "path1/")
+		require.NoError(t, err)
+		require.Len(t, entries, 2)
+		require.Len(t, subPaths, 2)
+	})
+
+	t.Run("invalid xml response when listing", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path2/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseXmlDecodeError)
+	})
+
+	t.Run("entries not sorted", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path3/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseEntriesNotSorted)
+	})
+
+	t.Run("sub paths not sorted", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path4/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseSubPathsNotSorted)
+	})
+
+	t.Run("sub paths incorrectly prefixed", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path5/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseSubPathsWrongPrefix)
+	})
+
+	t.Run("sub paths incorrectly prefixed", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path6/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseSubPathsWrongSuffix)
+	})
+
+	t.Run("sub paths contain incorrect characters", func(t *testing.T) {
+		_, _, err := s.ListEntries(ctx, "path7/")
+		require.ErrorIs(t, err, ErrInvalidResponse)
+		require.ErrorIs(t, err, ErrInvalidResponseSubPathMalicious)
+	})
+
+	t.Run("query with continuation token", func(t *testing.T) {
+		entries, subPaths, err := s.ListEntries(ctx, "path8/")
+		require.NoError(t, err)
+		require.Len(t, entries, 4)
+		require.Len(t, subPaths, 4)
 	})
 
 }
