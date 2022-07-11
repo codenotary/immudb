7d77fdd79521d2926d63ed75fbd5df8613785426
joe-mann <contact@joemann.co.uk>
2022-07-11 11:31:05 +0200

    chore: format tools.go.
    Signed-off-by: Joe Mann <joe@codenotary.com>

--- tools.go
+++ tools.go
@@ -3,11 +3,11 @@
 package tools
 
 import (
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "google.golang.org/grpc"
 	_ "github.com/golang/protobuf/proto"
 	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc"
 	_ "github.com/rakyll/statik"
+	_ "google.golang.org/grpc"
 )
