dd77894652f142352f08789bd0863358bb5cbe75
Bartłomiej Święcki <bart@codenotary.com>
2022-07-18 12:19:55 +0200

    chore(test/performance): Add basic IO stats
    Signed-off-by: Bartłomiej Święcki <bart@codenotary.com>

--- test/performance-test-suite/pkg/benchmarks/hwstats.go
+++ test/performance-test-suite/pkg/benchmarks/hwstats.go
@@ -27,58 +27,81 @@ type HWStats struct {
 	CPUKernelTimeFraction float64 `json:"cpuTimeKernelFraction"` // Fraction of the total CPU time that was spent on the kernel side
 	VMM                   uint64  `json:"vmm"`                   // Virtual memory used
 	RSS                   uint64  `json:"rss"`                   // Resident memory used
+	IOBytesRead           uint64  `json:"ioBytesRead"`           // Number of bytes read
+	IOBytesWrite          uint64  `json:"ioBytesWrite"`          // Number of bytes written
+	IOCallsRead           uint64  `json:"ioCallsRead"`           // Number of io read syscalls
+	IOCallsWrite          uint64  `json:"ioCallsWrite"`          // Number of io write syscalls
 }
 
 func (h *HWStats) String() string {
 	return fmt.Sprintf(
-		"CPUTime: %.2f, CPUKernelFrac: %.2f, VMM: %d, RSS: %d",
+		"CPUTime: %.2f, CPUKernelFrac: %.2f, VMM: %d, RSS: %d, Writes (bytes/calls): %d/%d, Reads (bytes/calls): %d/%d",
 		h.CPUTime,
 		h.CPUKernelTimeFraction,
 		h.VMM,
 		h.RSS,
+		h.IOBytesWrite,
+		h.IOCallsWrite,
+		h.IOBytesRead,
+		h.IOCallsRead,
 	)
 }
 
-type HWStatsGatherer struct {
-	ps           procfs.Proc
-	startCPUTime float64
-	startUTime   uint
-	startSTime   uint
+type HWStatsProber struct {
+	ps          procfs.Proc
+	initialStat procfs.ProcStat
+	initialIO   procfs.ProcIO
 }
 
-func NewHWStatsGatherer() (*HWStatsGatherer, error) {
+func NewHWStatsProber() (*HWStatsProber, error) {
 	ps, err := procfs.Self()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Couldn't initialize HW stats prober: %v", err)
 	}
 
-	initialStats, err := ps.Stat()
+	initialStat, err := ps.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't initialize HW stats prober: %v", err)
+	}
 
-	return &HWStatsGatherer{
-		ps:           ps,
-		startCPUTime: initialStats.CPUTime(),
-		startUTime:   initialStats.UTime,
-		startSTime:   initialStats.STime,
+	initialIO, err := ps.IO()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't initialize HW stats prober: %v", err)
+	}
+
+	return &HWStatsProber{
+		ps:          ps,
+		initialStat: initialStat,
+		initialIO:   initialIO,
 	}, nil
 }
 
-func (h *HWStatsGatherer) GetHWStats() (*HWStats, error) {
+func (h *HWStatsProber) GetHWStats() (*HWStats, error) {
 	stat, err := h.ps.Stat()
 	if err != nil {
 		return nil, err
 	}
 
-	uTime := stat.UTime - h.startUTime
-	sTime := stat.STime - h.startSTime
+	uTime := stat.UTime - h.initialStat.UTime
+	sTime := stat.STime - h.initialStat.STime
 	ktFrac := float64(0)
 	if uTime+sTime > 0 {
 		ktFrac = float64(sTime) / float64(sTime+uTime)
 	}
 
+	ioStat, err := h.ps.IO()
+	if err != nil {
+		return nil, err
+	}
+
 	return &HWStats{
-		CPUTime:               stat.CPUTime() - h.startCPUTime,
+		CPUTime:               stat.CPUTime() - h.initialStat.CPUTime(),
 		CPUKernelTimeFraction: ktFrac,
 		VMM:                   uint64(stat.VirtualMemory()),
 		RSS:                   uint64(stat.ResidentMemory()),
+		IOBytesRead:           ioStat.ReadBytes - h.initialIO.ReadBytes,
+		IOBytesWrite:          ioStat.WriteBytes - h.initialIO.WriteBytes,
+		IOCallsRead:           ioStat.SyscR - h.initialIO.SyscR,
+		IOCallsWrite:          ioStat.SyscW - h.initialIO.SyscW,
 	}, nil
 }
