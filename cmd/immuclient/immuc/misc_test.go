3f0b51352377655d1058b7e42d3954f603b923f8
Joe Mann <joe@codenotary.com>
2022-07-19 11:18:54 +0200

    feat: add test for HealthCheck.
    Signed-off-by: Joe Mann <joe@codenotary.com>

---
+++ cmd/immuclient/immuc/misc_test.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2022 CodeNotary, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package immuc_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/codenotary/immudb/cmd/cmdtest"
+	test "github.com/codenotary/immudb/cmd/immuclient/immuclienttest"
+	"github.com/codenotary/immudb/pkg/client/tokenservice"
+	"github.com/codenotary/immudb/pkg/server"
+	"github.com/codenotary/immudb/pkg/server/servertest"
+)
+
+func TestHealthCheck(t *testing.T) {
+	options := server.DefaultOptions().WithAuth(true)
+	bs := servertest.NewBufconnServer(options)
+
+	bs.Start()
+	defer bs.Stop()
+
+	defer os.RemoveAll(options.Dir)
+	defer os.Remove(".state-")
+
+	tkf := cmdtest.RandString()
+	ts := tokenservice.NewFileTokenService().WithTokenFileName(tkf)
+	ic := test.NewClientTest(&test.PasswordReader{
+		Pass: []string{"immudb"},
+	}, ts)
+	ic.Connect(bs.Dialer)
+	ic.Login("immudb")
+
+	if msg, err := ic.Imc.HealthCheck(nil); err != nil {
+		t.Fatal("HealthCheck fail", err)
+	} else if !strings.Contains(msg, "Health check OK") {
+		t.Fatalf("HealthCheck failed: %s", msg)
+	}
+}
