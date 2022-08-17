5d090635cf125e3ed8e6456b21160b25d07c175c
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2022-08-16 23:39:12 -0300

    chore((embedded/store): keep flush calls
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- embedded/store/immustore.go
+++ embedded/store/immustore.go
@@ -399,6 +399,7 @@ func OpenWith(path string, vLogs []appendable.Appendable, txLog, cLog appendable
 		WithFileMode(opts.FileMode).
 		WithFileSize(fileSize).
 		WithRetryableSync(opts.Synced).
+		WithAutoSync(true).
 		WithWriteBufferSize(opts.AHTOpts.WriteBufferSize).
 		WithSyncThld(opts.AHTOpts.SyncThld)
 
@@ -1029,6 +1030,12 @@ func (s *ImmuStore) appendData(entries []*EntrySpec, donec chan<- appendableResu
 		offsets[i] = encodeOffset(voff, vLogID)
 	}
 
+	err := vLog.Flush()
+	if err != nil {
+		donec <- appendableResult{nil, err}
+		return
+	}
+
 	donec <- appendableResult{offsets, nil}
 }
 
@@ -1404,6 +1411,11 @@ func (s *ImmuStore) performPreCommit(tx *Tx, ts int64, blTxID uint64) error {
 		return err
 	}
 
+	err = s.txLog.Flush()
+	if err != nil {
+		return err
+	}
+
 	if s.blBuffer == nil {
 		err = s.aht.ResetSize(s.preCommittedTxID)
 		if err != nil {
@@ -1437,6 +1449,11 @@ func (s *ImmuStore) performPreCommit(tx *Tx, ts int64, blTxID uint64) error {
 		}
 
 		_, _, err = s.cLog.Append(cb[:])
+		if err != nil {
+			return err
+		}
+
+		err = s.cLog.Flush()
 		if err != nil {
 			return err
 		}
