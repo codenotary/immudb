e6e3edab1da3ad97bf46a79f03556b62155f43ad
Bartłomiej Święcki <bart@codenotary.com>
2022-10-04 12:21:37 +0200

    Revert "DO NOT MERGE: Enable some more tests"
    This reverts commit 7f82242cc49bfd27a01e4ffb5056c651586f8a36.

--- pkg/integration/replication/synchronous_replication_test.go
+++ pkg/integration/replication/synchronous_replication_test.go
@@ -58,7 +58,7 @@ func (suite *SyncTestSuite) TestSyncFromMasterToAllFollowers() {
 	}
 }
 
-func (suite *SyncTestSuite) TestMasterRestart() {
+func (suite *SyncTestSuite) _TestMasterRestart() {
 	var txBeforeRestart *schema.TxHeader
 	suite.Run("commit before restarting primary", func() {
 
@@ -104,7 +104,7 @@ func (suite *SyncTestSuite) TestMasterRestart() {
 
 // TestPrecommitStateSync checks if the precommit state at master
 // and its followers are in sync during synchronous replication
-func (suite *SyncTestSuite) TestPrecommitStateSync() {
+func (suite *SyncTestSuite) _TestPrecommitStateSync() {
 	var (
 		masterState *schema.ImmutableState
 		err         error
