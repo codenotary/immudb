2dc7d79558778246cd43892285df6e84cad88825
Jeronimo Irazabal <jeronimo.irazabal@gmail.com>
2023-01-31 10:09:01 -0300

    test(pkg/integration/replication): increse replication timeout
    Signed-off-by: Jeronimo Irazabal <jeronimo.irazabal@gmail.com>

--- pkg/integration/replication/synchronous_replication_test.go
+++ pkg/integration/replication/synchronous_replication_test.go
@@ -375,12 +375,12 @@ func (suite *SyncTestRecoverySpeedSuite) TestReplicaRecoverySpeed() {
 		suite.Require().NoError(err)
 		suite.Require().Greater(state.TxId, txWritten, "Ensure enough TXs were written")
 
-		// Check if we can recover the cluster and perform write within the double the amount of time
+		// Check if we can recover the cluster and perform write within a reasonable amount of time
 		// that was needed for initial sampling. The replica that was initially stopped and now
 		// started has the same amount of transaction to grab from primary as the other one
 		// which should take the same amount of time as the initial write period or less
 		// (since the primary is not persisting data this time).
-		ctxTimeout, cancel := context.WithTimeout(ctx, samplingTime*2)
+		ctxTimeout, cancel := context.WithTimeout(ctx, samplingTime*4)
 		defer cancel()
 
 		suite.StartReplica(0) // 1 down
