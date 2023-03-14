1ec89b14e5049eb69a5962935c406555373a4cab
Farhan Khan <farhan@codenotary.com>
2023-03-14 09:49:52 +0530

    chore(pkg/server): integrate document functions with server apis

--- pkg/server/documents_operations.go
+++ pkg/server/documents_operations.go
@@ -23,45 +23,66 @@ import (
 )
 
 func (s *ImmuServer) DocumentInsert(ctx context.Context, req *documentschema.DocumentInsertRequest) (*documentschema.DocumentInsertResponse, error) {
-	_, err := s.getDBFromCtx(ctx, "DocumentInsert")
+	db, err := s.getDBFromCtx(ctx, "DocumentInsert")
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+	resp, err := db.CreateDocument(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (s *ImmuServer) DocumentSearch(ctx context.Context, req *documentschema.DocumentSearchRequest) (*documentschema.DocumentSearchResponse, error) {
-	_, err := s.getDBFromCtx(ctx, "DocumentSearch")
+	db, err := s.getDBFromCtx(ctx, "DocumentSearch")
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+	resp, err := db.GetDocument(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (s *ImmuServer) CollectionCreate(ctx context.Context, req *documentschema.CollectionCreateRequest) (*documentschema.CollectionCreateResponse, error) {
-	_, err := s.getDBFromCtx(ctx, "CollectionCreate")
+	db, err := s.getDBFromCtx(ctx, "CollectionCreate")
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+	resp, err := db.CreateCollection(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (s *ImmuServer) CollectionGet(ctx context.Context, req *documentschema.CollectionGetRequest) (*documentschema.CollectionGetResponse, error) {
-	_, err := s.getDBFromCtx(ctx, "CollectionGet")
+	db, err := s.getDBFromCtx(ctx, "CollectionGet")
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+	resp, err := db.GetCollection(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (s *ImmuServer) CollectionList(ctx context.Context, req *documentschema.CollectionListRequest) (*documentschema.CollectionListResponse, error) {
-	_, err := s.getDBFromCtx(ctx, "CollectionList")
+	db, err := s.getDBFromCtx(ctx, "CollectionList")
 	if err != nil {
 		return nil, err
 	}
-	return nil, nil
+	resp, err := db.ListCollections(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
+// TODO: implement
 func (s *ImmuServer) CollectionDelete(ctx context.Context, req *documentschema.CollectionDeleteRequest) (*documentschema.CollectionDeleteResponse, error) {
 	_, err := s.getDBFromCtx(ctx, "CollectionDelete")
 	if err != nil {
@@ -70,6 +91,7 @@ func (s *ImmuServer) CollectionDelete(ctx context.Context, req *documentschema.C
 	return nil, nil
 }
 
+// TODO: implement
 func (s *ImmuServer) DocumentAudit(ctx context.Context, req *documentschema.DocumentAuditRequest) (*documentschema.DocumentAuditResponse, error) {
 	_, err := s.getDBFromCtx(ctx, "DocumentAudit")
 	if err != nil {
@@ -78,6 +100,7 @@ func (s *ImmuServer) DocumentAudit(ctx context.Context, req *documentschema.Docu
 	return nil, nil
 }
 
+// TODO: implement
 func (s *ImmuServer) DocumentProof(ctx context.Context, req *documentschema.DocumentProofRequest) (*documentschema.DocumentProofResponse, error) {
 	_, err := s.getDBFromCtx(ctx, "DocumentProof")
 	if err != nil {
