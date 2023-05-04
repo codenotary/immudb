8b0435f7e5a9d40afbc676f95207dfdc720dea1f
Marco Sanchotene <marco@pontograu.com>
2023-05-04 12:57:48 -0300

    chore(test/documents): add conflicting files
    Signed-off-by: Marco Sanchotene <marco@codenotary.com>

---
+++ test/document_storage_tests/documents_tests/auth_test.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2023 Codenotary Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/codenotary/immudb/pkg/api/httpclient"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAuthentication(t *testing.T) {
+	client := getClient()
+
+	badLogin := "immudbXXX"
+	badPassword := "immudbXXX"
+	badDatabase := "defaultdbXXX"
+
+	response, err := client.OpenSessionWithResponse(context.Background(), httpclient.OpenSessionJSONRequestBody{
+		Username: &badLogin,
+		Password: &badPassword,
+		Database: &badDatabase,
+	})
+	require.NoError(t, err)
+	require.True(t, *response.JSONDefault.Message == "invalid user name or password")
+
+	defaultLogin := "immudb"
+	defaultPassword := "immudb"
+	defaultDatabase := "defaultdb"
+
+	response, err = client.OpenSessionWithResponse(context.Background(), httpclient.OpenSessionJSONRequestBody{
+		Username: &defaultLogin,
+		Password: &defaultPassword,
+		Database: &defaultDatabase,
+	})
+	require.NoError(t, err)
+	require.True(t, response.StatusCode() == 200)
+	require.True(t, len(*response.JSON200.SessionID) > 0)
+}
---
+++ test/document_storage_tests/documents_tests/documents_test.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2023 Codenotary Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/codenotary/immudb/pkg/api/httpclient"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateDocument(t *testing.T) {
+	client := getAuthorizedClient()
+
+	uuid := uuid.New()
+
+	documentToInsert := make(map[string]interface{})
+	documentToInsert["uuid"] = uuid.String()
+	documentToInsert["name"] = "John"
+	documentToInsert["surname"] = "Doe"
+	documentToInsert["age"] = 30
+
+	collectionName := getStandarizedRandomString()
+
+	err := createCollection(collectionName, client)
+	require.NoError(t, err)
+
+	req := httpclient.ModelDocumentInsertRequest{
+		Collection: &collectionName,
+		Document:   &documentToInsert,
+	}
+	response, err := client.DocumentInsertWithResponse(context.Background(), req)
+	require.NoError(t, err)
+	require.True(t, response.StatusCode() == 200)
+
+	documentId := response.JSON200.DocumentId
+
+	fieldName := "_id"
+	operator := httpclient.EQ
+
+	query := httpclient.ModelQuery{
+		Expressions: &[]httpclient.ModelQueryExpression{
+			{
+				FieldComparisons: &[]httpclient.ModelFieldComparison{
+					{
+						Field:    &fieldName,
+						Operator: &operator,
+						Value:    documentId,
+					},
+				},
+			},
+		},
+	}
+
+	page := int64(1)
+	perPage := int64(1)
+
+	searchReq := httpclient.ModelDocumentSearchRequest{
+		Collection: &collectionName,
+		Query:      &query,
+		Page:       &page,
+		PerPage:    &perPage,
+	}
+
+	searchResponse, err := client.DocumentSearchWithResponse(context.Background(), searchReq)
+	require.NoError(t, err)
+	fmt.Println(searchResponse.StatusCode())
+	require.True(t, searchResponse.StatusCode() == 200)
+
+	revisions := *searchResponse.JSON200.Revisions
+
+	firstDocument := (*revisions[0].Document)
+
+	require.Equal(t, *documentId, firstDocument["_id"])
+	require.Equal(t, float64(30), firstDocument["age"])
+	require.Equal(t, "John", firstDocument["name"])
+	require.Equal(t, "Doe", firstDocument["surname"])
+
+}
