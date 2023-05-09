65c769b2fce251cfd1af39971aaca7f1db2690a1
Simone Lazzaris <slazzaris@gmail.com>
2023-05-09 15:16:26 +0200

    Async replica

--- test/release_perf_test/runme.sh
+++ test/release_perf_test/runme.sh
@@ -46,5 +46,36 @@ fi # FIXME remove
 
 ## ASYNC REPLICATION
 
-docker-compose up -d immudb-async-main immudb-async-replica
-sleep 5
+STATS=()
+> /tmp/runme.log
+for BATCHSIZE in 1 10 100
+do
+	for WORKERS in 1 10 100
+	do
+	echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS" >> /tmp/runme.log
+	docker-compose up -d immudb-async-main immudb-async-replica
+	sleep 10
+	WLEN=$(expr length $WORKERS)
+	BATCHNUM=$((BASE_BATCHNUM/WLEN))
+	docker-compose run immudb-tools-kv -addr immudb-async-main -db perf -read-workers 0 -read-batchsize 0 -write-speed 0 -write-workers $WORKERS -write-batchsize $BATCHSIZE -write-batchnum $BATCHNUM -silent -summary 2>&1 | tee -a /tmp/runme.log
+	TXS=$(tail -n1 /tmp/runme.log|grep -F "TOTAL WRITE"|grep -Eo '[0-9]+ Txs/s'|cut -d ' ' -f 1)
+	STATS+=( $TXS )
+	echo "TXS: $TXS, STATS: ${STATS[*]}"
+	docker-compose down
+	done
+done
+# print resulting table
+IDX=0
+echo "#---"
+echo "clients	batchsize	repl.	Write TX/s	Write KV/s"
+for BATCHSIZE in 1 10 100
+do
+	for WORKERS in 1 10 100
+	do
+	TXS=${STATS[IDX]}
+	echo "$WORKERS	$BATCHSIZE		async	$TXS	$((TXS*BATCHSIZE))"
+	CSVLINE="$WORKERS,$BATCHSIZE,async,$TXS,$((TXS*BATCHSIZE))"
+	CSV_LINES+=( $CSVLINE )
+	IDX=$((IDX+1))
+	done
+done
