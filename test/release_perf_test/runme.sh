b11cbff70061d7def994ecf97e0fc11a02e68958
Simone Lazzaris <slazzaris@gmail.com>
2023-05-10 11:41:23 +0200

    Used -duration stresser2 flag

--- test/release_perf_test/runme.sh
+++ test/release_perf_test/runme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker-compose build immudb-perftest immudb-tools
 
-BASE_BATCHNUM=10000
+DURATION=60
 
 CSV_LINES=( 'client,batchsize,replication,Write TX/s,Write KV/s' )
 
@@ -29,7 +29,6 @@ function test_matrix() {
 	SRV=$1
 	ADDR=$2
 	REPL=$3
-
 	STATS=()
 	> /tmp/runme.log
 	for BATCHSIZE in 1 10 100
@@ -39,12 +38,14 @@ function test_matrix() {
 		echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS" >> /tmp/runme.log
 		docker-compose up -d $SRV
 		sleep 5
-		WLEN=$(expr length $WORKERS)
-		BATCHNUM=$((BASE_BATCHNUM/WLEN))
-		docker-compose run immudb-tools-kv -addr $ADDR -db perf -read-workers 0 -read-batchsize 0 -write-speed 0 -write-workers $WORKERS -write-batchsize $BATCHSIZE -write-batchnum $BATCHNUM -silent -summary 2>&1 | tee -a /tmp/runme.log
+		docker-compose run immudb-tools-kv -duration $DURATION\
+			-addr $ADDR -db perf \
+			-read-workers 0 -write-speed 0 \
+			-write-workers $WORKERS -write-batchnum $BATCHNUM \
+			-silent -summary 2>&1 | tee -a /tmp/runme.log
 		TXS=$(tail -n1 /tmp/runme.log|grep -F "TOTAL WRITE"|grep -Eo '[0-9]+ Txs/s'|cut -d ' ' -f 1)
 		STATS+=( $TXS )
-		CSVLINE="$WORKERS,$BATCHSIZE,$REPL,$TXS,$((TXS*BATCHSIZE))"
+		CSVLINE="$WORKERS;$BATCHSIZE;$REPL;$TXS;$((TXS*BATCHSIZE))"
 		CSV_LINES+=( $CSVLINE )
 		echo "TXS: $TXS, STATS: ${STATS[*]}"
 		docker-compose down
