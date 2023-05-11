4951d93893a94cb7783c869fbc8417768948244b
Simone Lazzaris <slazzaris@gmail.com>
2023-05-11 14:09:52 +0200

    Parametric duration, more info

--- test/release_perf_test/runme.sh
+++ test/release_perf_test/runme.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 docker-compose build immudb-perftest immudb-tools
 
-DURATION=60
+if [ -z "$1" ]
+then
+DURATION=600
+else
+DURATION=$1
+fi
 
 CSV_LINES=( 'client,batchsize,replication,Write TX/s,Write KV/s' )
 
@@ -34,7 +39,7 @@ function test_matrix_kv() {
 	do
 		for WORKERS in 1 10 100
 		do
-		echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS" >> /tmp/runme.log
+		echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS REPL $REPL" | tee -a /tmp/runme.log
 		docker-compose up -d $SRV
 		sleep 5
 		docker-compose run immudb-tools-kv \
@@ -47,7 +52,7 @@ function test_matrix_kv() {
 		CSVLINE="$WORKERS;$BATCHSIZE;$REPL;$TXS;$((TXS*BATCHSIZE))"
 		CSV_LINES+=( $CSVLINE )
 		echo "TXS: $TXS, STATS: ${STATS[*]}"
-		docker-compose down
+		docker-compose down --timeout 2
 		done
 	done
 	print_result "$REPL" "${STATS[@]}"
@@ -63,27 +68,29 @@ function test_matrix_sql() {
 	do
 		for WORKERS in 1 10 100
 		do
-		echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS" >> /tmp/runme.log
+		echo "BATCHSIZE $BATCHSIZE WORKERS $WORKERS REPL $REPL" | tee -a /tmp/runme.log
 		docker-compose up -d $SRV
 		sleep 5
-		docker-compose run immudb-tools-kv \
+		docker-compose run immudb-tools-sql \
 			-addr $ADDR -db perf -duration $DURATION \
 			-workers $WORKERS -txsize $BATCHSIZE \
 			2>&1 | tee -a /tmp/runme.log
-		TXS=$(tail -n1 /tmp/runme.log|grep -F "Total Writes"|grep -Eo '[0-9.]+ writes/s'|cut -d ' ' -f 1)
-		STATS+=( $TXS )
-		CSVLINE="$WORKERS;$BATCHSIZE;$REPL;$TXS;$((TXS/BATCHSIZE))"
+		WRS=$(tail -n1 /tmp/runme.log|grep -F "Total Writes"|grep -Eo '[0-9.]+ writes/s'|cut -d ' ' -f 1)
+		TXS=$(tail -n1 /tmp/runme.log|awk "/Total Writes/{print \$9/$BATCHSIZE}")
+		TRUNC_TRS=$(echo $WRS|grep -Eo '^[0-9]+')
+		STATS+=( $TRUNC_TRS )
+		CSVLINE="$WORKERS;$BATCHSIZE;$REPL;$TXS;$WRS"
 		CSV_LINES+=( $CSVLINE )
-		echo "TXS: $TXS, STATS: ${STATS[*]}"
-		docker-compose down
+		echo "TXS: $TXS, WRS: $WRS, STATS: ${STATS[*]}"
+		docker-compose down --timeout 2
 		done
 	done
 	print_result "$REPL" "${STATS[@]}"
 }
 
-test_matrix_kv "immudb-standalone" "immudb-standalone" "no"
-test_matrix_kv "immudb-async-main immudb-async-replica" "immudb-async-main" "async"
-test_matrix_kv "immudb-sync-main immudb-sync-replica" "immudb-sync-main" "sync"
+# test_matrix_kv "immudb-standalone" "immudb-standalone" "no"
+# test_matrix_kv "immudb-async-main immudb-async-replica" "immudb-async-main" "async"
+# test_matrix_kv "immudb-sync-main immudb-sync-replica" "immudb-sync-main" "sync"
 test_matrix_sql "immudb-standalone" "immudb-standalone" "no"
 test_matrix_sql "immudb-async-main immudb-async-replica" "immudb-async-main" "async"
 test_matrix_sql "immudb-sync-main immudb-sync-replica" "immudb-sync-main" "sync"
