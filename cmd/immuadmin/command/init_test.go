4311f0064d99b8a331c47f1a87773fb7402f162f
Georg Wechslberger <tauu@h2overclock.de>
2023-07-12 20:04:07 +0200

    test: unify the creation of test commandline for immuadmin

--- cmd/immuadmin/command/init_test.go
+++ cmd/immuadmin/command/init_test.go
@@ -19,9 +19,15 @@ package immuadmin
 import (
 	"testing"
 
+	"github.com/codenotary/immudb/pkg/auth"
 	"github.com/codenotary/immudb/pkg/client"
+	"github.com/codenotary/immudb/pkg/server"
+	"github.com/codenotary/immudb/pkg/server/servertest"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestOptions(t *testing.T) {
@@ -34,4 +40,49 @@ func TestOptionsMtls(t *testing.T) {
 	viper.Set("mtls", true)
 	opts := Options()
 	assert.IsType(t, &client.Options{}, opts)
+}
+
+type passwordReaderMock struct {
+	Counter int
+}
+
+func (pwr *passwordReaderMock) Read(msg string) ([]byte, error) {
+	var pw []byte
+	if pwr.Counter == 0 {
+		pw = []byte(auth.SysAdminPassword)
+	} else {
+		pw = []byte(`Passw0rd!-`)
+	}
+	pwr.Counter++
+	return pw, nil
+}
+
+// Initialize an immudb instance and prepare a command line to connect to it.
+func newTestCommandLine(t *testing.T) (*commandline, *cobra.Command) {
+	tempDir := t.TempDir()
+	options := server.DefaultOptions().WithAuth(true).WithDir(tempDir)
+	bs := servertest.NewBufconnServer(options)
+
+	err := bs.Start()
+	if err != nil {
+		t.Fatalf("starting Bufconn server for immudb failed: %v", err)
+	}
+	t.Cleanup(func() { bs.Stop() })
+
+	// Create a command line with the dial options to connect to the test server.
+	cmdl := NewCommandLine()
+	cmdl.dialOptions = []grpc.DialOption{
+		grpc.WithContextDialer(bs.Dialer), grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	pwr := &passwordReaderMock{}
+	cmdl.passwordReader = pwr
+
+	// Create command and execute it to initialize command line flags.
+	cmd, err := cmdl.NewCmd()
+	if err != nil {
+		t.Fatalf("initializing cobra command failed: %v", err)
+	}
+	cmdl.Register(cmd)
+
+	return cmdl, cmd
 }
