4a9e3f1e06c2fa4cbf9dcba201e8ea8d8d1b90b4
Georg Wechslberger <tauu@h2overclock.de>
2023-07-13 17:29:55 +0200

    style: move newTestCommandLine to cmd_test

--- cmd/immuadmin/command/init_test.go
+++ cmd/immuadmin/command/init_test.go
@@ -17,18 +17,11 @@ limitations under the License.
 package immuadmin
 
 import (
-	"io"
 	"testing"
 
-	"github.com/codenotary/immudb/pkg/auth"
 	"github.com/codenotary/immudb/pkg/client"
-	"github.com/codenotary/immudb/pkg/server"
-	"github.com/codenotary/immudb/pkg/server/servertest"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestOptions(t *testing.T) {
@@ -41,70 +34,4 @@ func TestOptionsMtls(t *testing.T) {
 	viper.Set("mtls", true)
 	opts := Options()
 	assert.IsType(t, &client.Options{}, opts)
-}
-
-type passwordReaderMock struct {
-	Counter   int
-	Passwords []string
-}
-
-func (pwr *passwordReaderMock) Read(msg string) (pw []byte, err error) {
-	if pwr.Counter < len(pwr.Passwords) {
-		pw = []byte(pwr.Passwords[pwr.Counter])
-	} else {
-		err = io.EOF
-	}
-	pwr.Counter++
-	return
-}
-func (pwr *passwordReaderMock) Reset() {
-	pwr.Counter = 0
-}
-
-// Initialize an immudb instance and prepare a command line to connect to it.
-func newTestCommandLine(t *testing.T) (*commandline, *cobra.Command) {
-	tempDir := t.TempDir()
-	options := server.DefaultOptions().WithAuth(true).WithDir(tempDir)
-	bs := servertest.NewBufconnServer(options)
-
-	err := bs.Start()
-	if err != nil {
-		t.Fatalf("starting Bufconn server for immudb failed: %v", err)
-	}
-	t.Cleanup(func() { bs.Stop() })
-
-	// Create a command line with the dial options to connect to the test server.
-	cmdl := NewCommandLine()
-	cmdl.dialOptions = []grpc.DialOption{
-		grpc.WithContextDialer(bs.Dialer), grpc.WithTransportCredentials(insecure.NewCredentials()),
-	}
-	// Initialize a mockup password reader with the default password.
-	pwr := &passwordReaderMock{
-		Counter:   0,
-		Passwords: []string{auth.SysAdminPassword},
-	}
-	cmdl.passwordReader = pwr
-
-	// Create command and execute it to initialize command line flags.
-	cmd, err := cmdl.NewCmd()
-	if err != nil {
-		t.Fatalf("initializing cobra command failed: %v", err)
-	}
-	cmd = cmdl.Register(cmd)
-
-	// register backup related commands
-	clb, err := newCommandlineBck(cmdl)
-	if err != nil {
-		t.Fatalf("initializing backup command failed: %v", err)
-	}
-	cmd = clb.Register(cmd)
-
-	// register hot backup related commands
-	clhb, err := newCommandlineHotBck(cmdl)
-	if err != nil {
-		t.Fatalf("initializing hot-backup command failed: %v", err)
-	}
-	cmd = clhb.Register(cmd)
-
-	return cmdl, cmd
 }
