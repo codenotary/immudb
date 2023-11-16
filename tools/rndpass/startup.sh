ffc5374562e02ff44bc7bc595a318159daebf5a1
Simone Lazzaris <slazzaris@gmail.com>
2023-11-16 16:34:02 +0100

    fix: source /etc/sysconfig/immudb on AWS EC2 startup
    Fix https://github.com/codenotary/immudb/issues/1868: user can just set
    /etc/sysconfig/immudb with desired env variables.

--- tools/rndpass/startup.sh
+++ tools/rndpass/startup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-
+if [ -f /etc/sysconfig/immudb ]
+then
+    source /etc/sysconfig/immudb
+fi
 if [ -z "$IMMUDB_ADMIN_PASSWORD" ]
 then
-   export IMMUDB_ADMIN_PASSWORD=`tr -cd '[:alnum:].,:;/@_=' < /dev/urandom|head -c 16`
+   IMMUDB_ADMIN_PASSWORD=`tr -cd '[:alnum:].,:;/@_=' < /dev/urandom|head -c 16`
    echo "Generated immudb password: $IMMUDB_ADMIN_PASSWORD"
 fi
-
+export IMMUDB_ADMIN_PASSWORD
 exec $@
