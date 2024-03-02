4dec0374962da640dff009bcca541188bf22482f
Stefano Scafiti <stefano.scafiti96@gmail.com>
2024-03-02 11:28:59 +0100

    Add TestExpiredEntryIsNotAccessibleFromHistory

--- embedded/store/deleted_entries_test.go
+++ embedded/store/deleted_entries_test.go
@@ -3,7 +3,9 @@ package store
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -86,30 +88,104 @@ func TestDeletedEntriesAreNotAccessibleFromHistory(t *testing.T) {
 
 	valRefs, _, err := immuStore.History(key, 0, false, nRecordsBeforeDelete)
 	require.NoError(t, err)
-
-	assertValuesAreVisible(t, valRefs)
+	assertValuesAreVisible(t, valRefs, false)
 
 	deleteKey(t, immuStore, key)
 	setKeys(t, immuStore, key, nRecordsAfterDelete)
 
-	// requested deleted records + non deleted records
-	valRefs, _, err = immuStore.History(key, uint64(nRecordsBeforeDelete/2), false, nRecordsBeforeDelete+1+nRecordsAfterDelete)
-	require.NoError(t, err)
+	t.Run("requesting deleted records only", func(t *testing.T) {
+		valRefs, _, err = immuStore.History(key, uint64(nRecordsBeforeDelete/2), false, nRecordsBeforeDelete/2)
+		require.NoError(t, err)
 
-	assertValuesAreDeleted(t, valRefs[:(nRecordsBeforeDelete/2)])
+		assertValuesAreDeleted(t, valRefs)
 
-	value, err := valRefs[nRecordsBeforeDelete/2].Resolve()
-	require.NoError(t, err)
-	require.Nil(t, value)
+		valRefs, _, err = immuStore.History(key, uint64(nRecordsAfterDelete+1), true, nRecordsBeforeDelete)
+		require.NoError(t, err)
 
-	assertValuesAreVisible(t, valRefs[(nRecordsBeforeDelete/2+1):])
+		assertValuesAreDeleted(t, valRefs)
+	})
+
+	t.Run("requesting deleted and non deleted records", func(t *testing.T) {
+		valRefs, _, err = immuStore.History(key, uint64(nRecordsBeforeDelete/2), false, nRecordsBeforeDelete+1+nRecordsAfterDelete)
+		require.NoError(t, err)
+
+		assertValuesAreDeleted(t, valRefs[:(nRecordsBeforeDelete/2)])
+		assertIsDeleteEntry(t, valRefs[nRecordsBeforeDelete/2])
+		assertValuesAreVisible(t, valRefs[(nRecordsBeforeDelete/2+1):], false)
+
+		valRefs, _, err = immuStore.History(key, 0, true, nRecordsBeforeDelete+1+nRecordsAfterDelete)
+		require.NoError(t, err)
+
+		assertValuesAreVisible(t, valRefs[:nRecordsAfterDelete], true)
+		assertIsDeleteEntry(t, valRefs[nRecordsAfterDelete])
+		assertValuesAreDeleted(t, valRefs[nRecordsAfterDelete+1:])
+	})
 }
 
-func assertValuesAreVisible(t *testing.T, valRefs []ValueRef) {
+func assertIsDeleteEntry(t *testing.T, valRef ValueRef) {
+	require.NotNil(t, valRef.KVMetadata())
+	require.True(t, valRef.KVMetadata().Deleted())
+
+	value, err := valRef.Resolve()
+	require.NoError(t, err)
+	require.Nil(t, value)
+}
+
+func TestExpiredEntryIsNotAccessibleFromHistory(t *testing.T) {
+	var (
+		key      = []byte("test-key")
+		nRecords = 100
+	)
+
+	immuStore, err := Open(t.TempDir(), DefaultOptions())
+	require.NoError(t, err)
+	require.NotNil(t, immuStore)
+
+	defer immustoreClose(t, immuStore)
+
+	expiredRecordIndex := rand.Intn(nRecords)
+	setKeys(t, immuStore, key, expiredRecordIndex)
+
+	tx, err := immuStore.NewWriteOnlyTx(context.Background())
+	require.NoError(t, err)
+
+	md := NewKVMetadata()
+	err = md.ExpiresAt(time.Now().Add(-time.Second))
+	require.NoError(t, err)
+
+	err = tx.Set(key, md, []byte("expired-value"))
+	require.NoError(t, err)
+
+	_, err = tx.Commit(context.Background())
+	require.NoError(t, err)
+
+	setKeys(t, immuStore, key, nRecords-expiredRecordIndex-1)
+
+	valRefs, _, err := immuStore.History(key, 0, false, nRecords)
+	require.NoError(t, err)
+	require.Len(t, valRefs, nRecords)
+
+	for i, valRef := range valRefs {
+		value, err := valRef.Resolve()
+		if i == expiredRecordIndex {
+			require.ErrorIs(t, err, ErrValueDeleted)
+			require.Nil(t, value)
+		} else {
+			require.NoError(t, err)
+		}
+	}
+}
+
+func assertValuesAreVisible(t *testing.T, valRefs []ValueRef, reverse bool) {
 	for i, valRef := range valRefs {
 		value, err := valRef.Resolve()
 		require.NoError(t, err)
-		require.Equal(t, []byte(fmt.Sprintf("test-value-%d", i)), value)
+
+		expectedIdx := i
+		if reverse {
+			expectedIdx = len(valRefs) - 1 - i
+		}
+		require.Equal(t, []byte(fmt.Sprintf("test-value-%d", expectedIdx)), value)
 	}
 }
 
