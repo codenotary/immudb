ad7b13a829e2fde31e545a4a91b0f463621f91bc
Stefano Scafiti <stefano.scafiti96@gmail.com>
2024-05-21 17:29:37 +0200

    Prevent replication with different replica and primary server versions
    Signed-off-by: Stefano Scafiti <stefano.scafiti96@gmail.com>

--- embedded/document/engine.go
+++ embedded/document/engine.go
@@ -5,7 +5,7 @@ SPDX-License-Identifier: BUSL-1.1
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    https://mariadb.com/bsl11/
+	https://mariadb.com/bsl11/
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
