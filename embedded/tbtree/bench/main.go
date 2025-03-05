7eb3b3a37bf464d9910ca60c61c6a2dd4693bb74
Stefano Scafiti <stefano.scafiti96@gmail.com>
2025-03-05 17:10:10 +0100

    add stats

--- embedded/tbtree/bench/main.go
+++ embedded/tbtree/bench/main.go
@@ -13,37 +13,40 @@ import (
 func main() {
 	size := 1024 * 1024 * 128
 
-	fmt.Println("Pages Per Chunk | Alloc/sec | Time (ms)")
+	fmt.Println("| NumChunks | Alloc time (ms) |")
 	fmt.Println("------------------------------------------------")
 
-	n := 10000
+	const n = 10000
 
-	numThreads := 64
+	const numThreads = 64
 
 	// Test with increasing number of goroutines
-	for pages := 1; pages <= 256; pages *= 2 {
+	for chunkSizePages := 1; chunkSizePages <= 256; chunkSizePages *= 2 {
 		var wg sync.WaitGroup
 		var allocCount atomic.Uint32
 
-		swb := tbtree.NewSharedWriteBuffer(size, pages*tbtree.PageSize)
+		swb := tbtree.NewSharedWriteBuffer(size, chunkSizePages*tbtree.PageSize)
 
 		buffers := make([]*tbtree.WriteBuffer, numThreads)
 		for t := 0; t < 64; t++ {
-			wb, err := tbtree.NewWriteBuffer(swb, pages*tbtree.PageSize, size)
+			wb, err := tbtree.NewWriteBuffer(swb, chunkSizePages*tbtree.PageSize, size)
 			if err != nil {
 				panic(err)
 			}
 			buffers[t] = wb
 		}
 
-		start := time.Now()
+		var times [numThreads]time.Duration
 
 		wg.Add(numThreads)
 		for t := 0; t < numThreads; t++ {
-			go func(wb *tbtree.WriteBuffer) {
+			go func(wb *tbtree.WriteBuffer, nTh int) {
 				defer wg.Done()
 				for i := 0; i < n; i++ {
+					start := time.Now()
 					_, _, err := wb.AllocLeafPage()
+					times[nTh] += time.Since(start)
+
 					if errors.Is(err, tbtree.ErrWriteBufferFull) || errors.Is(err, tbtree.ErrNoChunkAvailable) {
 						wb.Reset()
 					}
@@ -52,14 +55,32 @@ func main() {
 						allocCount.Add(1)
 					}
 				}
-			}(buffers[t])
+			}(buffers[t], t)
 		}
 		wg.Wait()
 
 		swb.ReleaseAll()
 
-		// Calculate and display results
-		elapsed := time.Since(start).Milliseconds()
-		fmt.Printf("%7d | %11d | %9d\n", pages, allocCount.Load(), elapsed)
+		avg, std := computeStats(times[:])
+		fmt.Printf("| %d | %f | %f\n", chunkSizePages, avg, std)
 	}
+}
+
+func computeStats(times []time.Duration) (float64, float64) {
+	avgTime := avg(times)
+
+	std := float64(0)
+	for _, v := range times {
+		diff := (float64(v.Milliseconds()) - avgTime)
+		std += diff * diff
+	}
+	return avgTime, std
+}
+
+func avg(times []time.Duration) float64 {
+	x := time.Duration(0)
+	for _, v := range times {
+		x += v
+	}
+	return float64(x.Milliseconds()) / float64(len(times))
 }
